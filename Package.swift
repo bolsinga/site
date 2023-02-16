d41e87b39a5d3d0a923cb85f2c821b5ddcb5f773
Greg Bolsinga <bolsinga@mac.com>
2023-02-15 16:28:07 -0800

    use swift arg parser (#10)

--- Package.swift
+++ Package.swift
@@ -11,8 +11,15 @@ let package = Package(
     .library(name: "json", targets: ["json"]),
     .executable(name: "site_tool", targets: ["site_tool"]),
   ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2")
+  ],
   targets: [
     .target(name: "json"),
-    .executableTarget(name: "site_tool", dependencies: [.byName(name: "json")]),
+    .executableTarget(
+      name: "site_tool",
+      dependencies: [
+        .byName(name: "json"), .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]),
   ]
 )
