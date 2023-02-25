193595ddc35352b6fa96098c6a28f6de341d7220
Greg Bolsinga <bolsinga@mac.com>
2023-02-25 14:09:42 -0800

    Build debug strings with Array.joined (#41)

--- Sources/Site/Music/Music+Description.swift
+++ Sources/Site/Music/Music+Description.swift
@@ -9,92 +9,95 @@ import Foundation
 
 extension Music {
   public func description(for show: Show) -> String {
-    var description = "\(show.id) -"
-
-    description = description + " \(Music.description(for: show.date))"
+    var parts: [String] = []
+    parts.append(show.id)
+    parts.append(Music.description(for: show.date))
 
     var artistList = "[Unknown Artists]"
     do {
       artistList = try self.artistsForShow(show).map { $0.name }.joined(separator: ", ")
     } catch {
     }
-    description = description + ": \(artistList)"
+    parts.append(artistList)
 
     var venueName = "[Unknown Venue]"
     do {
       venueName = try self.venueForShow(show).name
     } catch {}
-    description = description + ": \(venueName)"
+    parts.append(venueName)
 
     if let comment = show.comment {
-      description = description + ": \(comment.prefix(10))"
-      if comment.count > 10 {
-        description = description + "…"
-      }
+      parts.append("\(comment.prefix(10))\(comment.count > 10 ?"…" :"")")
     }
 
-    return description
+    return parts.joined(separator: ": ")
   }
 
   public func description(for album: Album) -> String {
-    var description = "\(album.id) -"
+    var parts: [String] = []
+    parts.append(album.id)
 
     if let release = album.release {
-      description = description + " \(Music.description(for: release))"
+      parts.append(Music.description(for: release))
     }
 
     if let compilation = album.compilation, compilation {
-      description = description + " [Compilation]"
+      parts.append("[Compilation]")
     }
 
     if let artist = self.artistForAlbum(album) {
-      description = description + " \(artist.name):"
+      parts.append(artist.name)
     }
-    description = description + " \(album.title)"
+    parts.append(album.title)
 
-    return description
+    return parts.joined(separator: ": ")
   }
 
   public func description(for artist: Artist) -> String {
-    var description = "\(artist.id) - \(artist.name)"
+    var parts: [String] = []
+    parts.append(artist.id)
+    parts.append(artist.name)
 
     if let sortname = artist.sortname {
-      description = description + " (\(sortname))"
+      parts.append("(\(sortname))")
     }
 
     do {
       let albumList = try self.albumsForArtist(artist).map { $0.title }.joined(separator: ", ")
       if !albumList.isEmpty {
-        description = description + " [\(albumList)]"
+        parts.append("[\(albumList)]")
       }
     } catch {}
 
-    return description
+    return parts.joined(separator: ": ")
   }
 
   public func description(for location: Location) -> String {
-    var description = "\(location.city), \(location.state)"
+    var parts: [String] = []
+    parts.append("\(location.city), \(location.state)")
 
     if let street = location.street {
-      description = description + " (\(street))"
+      parts.append("(\(street))")
     }
 
     if let url = location.web {
-      description = description + ": \(url.absoluteString)"
+      parts.append(url.absoluteString)
     }
-    return description
+    return parts.joined(separator: ": ")
   }
 
   public func description(for venue: Venue) -> String {
-    var description = "\(venue.id) - \(venue.name)"
+    var parts: [String] = []
+    parts.append(venue.id)
+    parts.append(venue.name)
 
     if let sortname = venue.sortname {
-      description = description + " (\(sortname))"
+      parts.append("(\(sortname)")
     }
 
-    description = description + ": \(self.description(for: venue.location))"
+    parts.append(self.description(for: venue.location))
 
-    return description
+    return parts.joined(separator: ": ")
   }
 
   public static func description(for partialDate: PartialDate) -> String {
