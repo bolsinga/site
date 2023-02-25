bdbac6aee81df2adca48c8fb28121c83fa5de9ad
Greg Bolsinga <bolsinga@mac.com>
2023-02-25 09:54:41 -0800

    Improve Show comparison (#40)
    - if dates are the same (usually unknown in my dataset), sort by venue name if available, else by headliner.

--- Sources/Site/Music/Music+Comparator.swift
+++ Sources/Site/Music/Music+Comparator.swift
@@ -38,4 +38,22 @@ extension Music {
     assert(rhs.release == nil)
     return false
   }
+
+  public func showCompare(lhs: Show, rhs: Show) -> Bool {
+    if lhs.date == rhs.date {
+      if let lhVenue = try? self.venueForShow(lhs), let rhVenue = try? self.venueForShow(rhs) {
+        if lhVenue == rhVenue {
+          if let lhArtists = try? self.artistsForShow(lhs),
+            let rhArtists = try? self.artistsForShow(rhs)
+          {
+            if let lhHeadliner = lhArtists.first, let rhHeadliner = rhArtists.first {
+              return libraryCompare(lhs: lhHeadliner, rhs: rhHeadliner)
+            }
+          }
+        }
+        return libraryCompare(lhs: lhVenue, rhs: rhVenue)
+      }
+    }
+    return lhs.date < rhs.date
+  }
 }
