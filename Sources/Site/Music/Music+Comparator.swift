26ed8a76cf593aac8d15810c8905a7ebd1026f2f
Greg Bolsinga <bolsinga@mac.com>
2023-02-27 10:04:54 -0800

    Fix logic for sorting Albums (#56)

--- Sources/Site/Music/Music+Comparator.swift
+++ Sources/Site/Music/Music+Comparator.swift
@@ -8,35 +8,54 @@
 import Foundation
 
 extension Music {
-  public func albumCompare(lhs: Album, rhs: Album) -> Bool {
-    if let lhRelease = lhs.release, let rhRelease = rhs.release {
-      if lhRelease == rhRelease {
-        let lhArtist = self.artistForAlbum(lhs)
-        let rhArtist = self.artistForAlbum(rhs)
+  internal func artistAndTitleCompare(lhs: Album, rhs: Album) -> Bool {
+    let lhArtist = self.artistForAlbum(lhs)
+    let rhArtist = self.artistForAlbum(rhs)
 
-        if let lhArtist, let rhArtist {
-          if lhArtist == rhArtist {
-            return libraryCompare(lhs: lhs.title, rhs: rhs.title)
-          }
-          return libraryCompare(lhs: lhArtist, rhs: rhArtist)
+    if let lhArtist, let rhArtist {
+      // Both have artists.
+      if lhArtist == rhArtist {
+        // Artists are equal, so compare by album title.
+        if lhs.title == rhs.title {
+          // Titles are equal, so fallback to id.
+          return lhs.id < rhs.id
         }
-
-        if lhArtist == nil {
-          return true
-        }
-
-        assert(rhArtist == nil)
-        return false
+        return libraryCompare(lhs: lhs.title, rhs: rhs.title)
       }
+      // Compare by artists.
+      return libraryCompare(lhs: lhArtist, rhs: rhArtist)
+    }
+
+    if lhArtist == nil, rhArtist == nil {
+      // Both do not have artists, so compare just by title.
+      return libraryCompare(lhs: lhs.title, rhs: rhs.title)
+    }
+
+    // Now lhs or rhs is nil. If lhs is nil it sorts before the non nil rhs
+    return lhArtist == nil
+  }
+
+  public func albumCompare(lhs: Album, rhs: Album) -> Bool {
+    let lhRelease = lhs.release
+    let rhRelease = rhs.release
+
+    if let lhRelease, let rhRelease {
+      // Both have dates.
+      if lhRelease == rhRelease {
+        // Dates are equal, so compare just by artist and title.
+        return artistAndTitleCompare(lhs: lhs, rhs: rhs)
+      }
+      // Compare by dates.
       return lhRelease < rhRelease
     }
 
-    if lhs.release == nil {
-      return true
+    if lhRelease == nil, rhRelease == nil {
+      // Both do not have dates, so compare just by artist and title.
+      return artistAndTitleCompare(lhs: lhs, rhs: rhs)
     }
 
-    assert(rhs.release == nil)
-    return false
+    // Now lhs or rhs is nil. If lhs is nil it sorts before the non nil rhs
+    return lhs.release == nil
   }
 
   public func showCompare(lhs: Show, rhs: Show) -> Bool {
