c43b6c5df543a0e773fc792ae5a469a33cef1d85
Greg Bolsinga <bolsinga@mac.com>
2023-03-23 19:36:24 -0700

    Do not show albums
    - just starting with shows

--- Sources/Site/Music/UI/ArtistView.swift
+++ Sources/Site/Music/UI/ArtistView.swift
@@ -7,28 +7,9 @@
 
 import SwiftUI
 
-extension Album {
-  var formattedTitle: String {
-    var parts = [String]()
-    parts.append(title)
-    if let release {
-      parts.append("(\(Music.description(for: release)))")
-    }
-    return parts.joined(separator: " ")
-  }
-}
-
 struct ArtistView: View {
   let music: Music
   let artist: Artist
-
-  private var albums: [Album] {
-    do {
-      return try music.albumsForArtist(artist).sorted(by: music.albumCompare(lhs:rhs:))
-    } catch {
-      return []
-    }
-  }
 
   private var shows: [Show] {
     return music.showsForArtist(artist).sorted(by: music.showCompare(lhs:rhs:))
@@ -38,15 +19,6 @@ struct ArtistView: View {
     VStack(alignment: .leading) {
       Text(artist.name)
         .font(.title)
-      if !albums.isEmpty {
-        Divider()
-        ForEach(albums, id: \.id) { album in
-          HStack {
-            Text(album.formattedTitle)
-              .padding(2)
-          }
-        }
-      }
       if !shows.isEmpty {
         Divider()
         ForEach(shows, id: \.id) { show in
@@ -65,27 +37,7 @@ struct ArtistView_Previews: PreviewProvider {
         city: "San Francisco", web: URL(string: "http://www.amoeba.com"), street: "1855 Haight St.",
         state: "CA"), name: "Amoeba Records")
 
-    let album1 = Album(
-      id: "a1",
-      performer: "ar0",
-      release: PartialDate(year: 1970),
-      songs: [],
-      title: "Another Title")
-
-    let album2 = Album(
-      id: "a0",
-      performer: "ar0",
-      release: PartialDate(year: 1970),
-      songs: [],
-      title: "Album Title")
-
-    let album3 = Album(
-      id: "a2",
-      performer: "ar0",
-      songs: [],
-      title: "Unknown Title")
-
-    let artist1 = Artist(albums: [album1.id, album2.id, album3.id], id: "ar0", name: "An Artist")
+    let artist1 = Artist(id: "ar0", name: "An Artist")
 
     let artist2 = Artist(id: "ar1", name: "Live Only Band")
 
@@ -102,7 +54,7 @@ struct ArtistView_Previews: PreviewProvider {
       date: PartialDate(), id: "sh17", venue: venue.id)
 
     let music = Music(
-      albums: [album1, album2, album3],
+      albums: [],
       artists: [artist1],
       relations: [],
       shows: [show1, show2, show3],
