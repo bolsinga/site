5a968b918a890f42459cdc493f38fc233d5c5a79
Greg Bolsinga <bolsinga@mac.com>
2023-03-24 14:39:16 -0700

    Put Music on the Environment

---
+++ Sources/Site/Music/Music+Environment.swift
@@ -0,0 +1,24 @@
+//
+//  Music+Environment.swift
+//
+//
+//  Created by Greg Bolsinga on 3/24/23.
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+  var music: Music {
+    get {
+      return self[MusicKey.self]
+    }
+    set {
+      self[MusicKey.self] = newValue
+    }
+  }
+}
+
+public struct MusicKey: EnvironmentKey {
+  public static let defaultValue: Music = Music(
+    albums: [], artists: [], relations: [], shows: [], songs: [], timestamp: .now, venues: [])
+}
