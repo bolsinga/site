bfb13d3e9c120383012716c5363f5f29ae452269
Greg Bolsinga <bolsinga@mac.com>
2023-03-26 15:53:59 -0700

    Add related artists to ArtistDetail (#87)

--- Sources/Site/Music/Music+Relations.swift
+++ Sources/Site/Music/Music+Relations.swift
@@ -13,4 +13,10 @@ extension Music {
       relations.filter { $0.members.contains(item.id) }.flatMap { $0.members }.contains($0.id)
     }.filter { $0.id != item.id }
   }
+
+  func related(_ item: Artist) -> [Artist] {
+    return artists.filter {
+      relations.filter { $0.members.contains(item.id) }.flatMap { $0.members }.contains($0.id)
+    }.filter { $0.id != item.id }
+  }
 }
