09fd615174d5eff47d9a31e30237d27550ba0b1e
Greg Bolsinga <bolsinga@mac.com>
2023-04-06 18:54:03 -0700

    Each TabItem is a NavigationStack
    - Fixes the titles

--- Sources/Site/Music/UI/NavigateTab.swift
+++ Sources/Site/Music/UI/NavigateTab.swift
@@ -14,29 +14,35 @@ public struct NavigateTab: View {
   }
 
   public var body: some View {
-    NavigationStack {
-      TabView {
+    TabView {
+      NavigationStack {
         ShowYearList(shows: music.shows.sorted(by: music.showCompare(lhs:rhs:)))
-          .tabItem {
-            Label(
-              String(localized: "Shows", bundle: .module, comment: "Title Of the Show List Tab"),
-              systemImage: "person.and.background.dotted")
-          }
-        VenueList(venues: music.venues.sorted(by: libraryCompare(lhs:rhs:)))
-          .tabItem {
-            Label(
-              String(localized: "Venues", bundle: .module, comment: "Title Of the Venue List Tab"),
-              systemImage: "music.note.house")
-          }
-        ArtistList(artists: music.artistsWithShows().sorted(by: libraryCompare(lhs:rhs:)))
-          .tabItem {
-            Label(
-              String(
-                localized: "Artists", bundle: .module, comment: "Title Of the Artist List Tab"),
-              systemImage: "music.mic")
-          }
+          .musicDestinations()
       }
-      .musicDestinations()
+      .tabItem {
+        Label(
+          String(localized: "Shows", bundle: .module, comment: "Title Of the Show List Tab"),
+          systemImage: "person.and.background.dotted")
+      }
+      NavigationStack {
+        VenueList(venues: music.venues.sorted(by: libraryCompare(lhs:rhs:)))
+          .musicDestinations()
+      }
+      .tabItem {
+        Label(
+          String(localized: "Venues", bundle: .module, comment: "Title Of the Venue List Tab"),
+          systemImage: "music.note.house")
+      }
+      NavigationStack {
+        ArtistList(artists: music.artistsWithShows().sorted(by: libraryCompare(lhs:rhs:)))
+          .musicDestinations()
+      }
+      .tabItem {
+        Label(
+          String(
+            localized: "Artists", bundle: .module, comment: "Title Of the Artist List Tab"),
+          systemImage: "music.mic")
+      }
     }
   }
 }
