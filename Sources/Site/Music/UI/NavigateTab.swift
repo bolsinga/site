7c4bf87ca45c2b3f6f8a50a2bd044ce679ec6290
Greg Bolsinga <bolsinga@mac.com>
2023-04-10 10:34:36 -0700

    Add back ArtistList that uses LibraryComparableList

--- Sources/Site/Music/UI/NavigateTab.swift
+++ Sources/Site/Music/UI/NavigateTab.swift
@@ -39,14 +39,8 @@ public struct NavigateTab: View {
           systemImage: "music.note.house")
       }
       NavigationStack {
-        LibraryComparableList(items: music.artistsWithShows().sorted(by: libraryCompare(lhs:rhs:)))
-        {
-          String(
-            localized: "\(music.showsForArtist($0).count) Show(s)", bundle: .module,
-            comment: "Value for the Artist # of Shows.")
-        }
-        .navigationTitle(Text("Artists", bundle: .module, comment: "Title for the Artist Detail"))
-        .musicDestinations()
+        ArtistList(artists: music.artistsWithShows().sorted(by: libraryCompare(lhs:rhs:)))
+          .musicDestinations()
       }
       .tabItem {
         Label(
