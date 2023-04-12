1ce6c75ac639aebab8b7395e4eb578723e01d318
Greg Bolsinga <bolsinga@mac.com>
2023-04-12 10:27:17 -0700

    Move Library Sorting Helpers to extensions on String (#146)
    - convert them from funcs to properties too.

--- Sources/Site/Utility/LibraryComparator.swift
+++ Sources/Site/Utility/LibraryComparator.swift
@@ -6,63 +6,14 @@
 //
 
 import Foundation
-import RegexBuilder
 
 public func libraryCompare(lhs: any LibraryComparable, rhs: any LibraryComparable) -> Bool {
   return libraryCompare(lhs: lhs.librarySortToken, rhs: rhs.librarySortToken)
 }
 
-private func removeCommonInitialWords(_ string: String) -> String {
-  let regex = Regex {
-    Optionally {
-      ChoiceOf {
-        "The"
-        "A"
-        "An"
-      }
-      OneOrMore {
-        .whitespace
-      }
-    }
-  }.ignoresCase()
-
-  let result = string.trimmingPrefix(regex)
-  return String(result)
-}
-
-internal func removeCommonInitialPunctuation(_ string: String) -> String {
-  var result = removeCommonInitialWords(string)
-
-  let regex = Regex {
-    Optionally {
-      ZeroOrMore {
-        .word.inverted
-      }
-    }
-    Capture {
-      OneOrMore {
-        .word
-        Optionally {
-          .whitespace
-        }
-      }
-    }
-    Optionally {
-      ZeroOrMore {
-        .word.inverted
-      }
-    }
-  }
-
-  if let match = result.wholeMatch(of: regex) {
-    result = String(match.output.1)
-  }
-  return result
-}
-
 internal func libraryCompare(lhs: String, rhs: String) -> Bool {
-  let lhSort = removeCommonInitialPunctuation(lhs)
-  let rhSort = removeCommonInitialPunctuation(rhs)
+  let lhSort = lhs.removeCommonInitialPunctuation
+  let rhSort = rhs.removeCommonInitialPunctuation
 
   var options = String.CompareOptions()
   options.insert(.caseInsensitive)
