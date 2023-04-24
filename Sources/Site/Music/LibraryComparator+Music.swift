a2f277b9552396ea280baa94795d676865d97e18
Greg Bolsinga <bolsinga@mac.com>
2023-04-24 09:59:44 -0700

    add async let. haha

--- Sources/Site/Music/LibraryComparator+Music.swift
+++ Sources/Site/Music/LibraryComparator+Music.swift
@@ -9,13 +9,13 @@ import Foundation
 
 extension LibraryComparator {
   public static func create(music: Music) async -> LibraryComparator {
-    let tokenMap = music.artists.reduce(
+    async let tokenMap = music.artists.reduce(
       into: music.venues.reduce(into: [:]) {
         $0[$1.id] = $1.librarySortToken
       }
     ) {
       $0[$1.id] = $1.librarySortToken
     }
-    return LibraryComparator(tokenMap: tokenMap)
+    return LibraryComparator(tokenMap: await tokenMap)
   }
 }
