eb36b750919c278bb59cba79b463f2d3b5029e52
Greg Bolsinga <bolsinga@mac.com>
2023-04-25 13:10:55 -0700

    Add String.librarySection extension (#171)

--- Sources/Site/Utility/LibrarySectioner.swift
+++ Sources/Site/Utility/LibrarySectioner.swift
@@ -9,18 +9,6 @@ import Foundation
 
 struct LibrarySectioner {
   func librarySection(_ item: any LibraryComparable) -> LibrarySection {
-    return librarySection(item.librarySortString)
-  }
-
-  internal func librarySection(_ string: String) -> LibrarySection {
-    let pfx = string.removeCommonInitialPunctuation.prefix(1)
-    if let first = pfx.first {
-      if first.isNumber {
-        return .numeric
-      } else if first.isPunctuation {
-        return .punctuation
-      }
-    }
-    return .alphabetic(String(pfx).uppercased())
+    item.librarySortString.librarySection
   }
 }
