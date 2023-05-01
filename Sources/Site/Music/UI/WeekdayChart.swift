262878bdfa87e0cc2dac6d69defcfc600ac3b9a5
Greg Bolsinga <bolsinga@mac.com>
2023-04-30 21:02:17 -0700

    Prepopulate all weekdays so that they are all shown (with 0 if necessary)

--- Sources/Site/Music/UI/WeekdayChart.swift
+++ Sources/Site/Music/UI/WeekdayChart.swift
@@ -10,11 +10,30 @@ import SwiftUI
 
 private let WeekdayChartFormat = Date.FormatStyle.dateTime.weekday(.abbreviated)
 
+// weekday as int: (weekday, int) as formatted localized string : count
+private var WeekdayAbbreviationsGetter: [Int: (String, Int)] {
+  var result = [Int: (String, Int)]()
+  let dateComponents = DateComponents(calendar: Calendar.current, year: 2023, month: 4, day: 23)  // start with known Sunday
+  var date = dateComponents.date
+
+  for weekday in 1...7 {
+    guard date != nil else { fatalError("need to be able to get abbreviated months") }
+
+    result[weekday] = (WeekdayChartFormat.format(date!), 0)
+
+    // go to next day
+    date = Calendar.autoupdatingCurrent.date(byAdding: .weekday, value: 1, to: date!)
+  }
+  return result
+}
+
+private let WeekdayAbbreviations = WeekdayAbbreviationsGetter
+
 struct WeekdayChart: View {
   let dates: [Date]
 
   private var computeWeekdayCounts: [Int: (String, Int)] {  // weekday as int: (weekday as string, count of that weekday)
-    return dates.reduce(into: [Int: (String, Int)]()) {
+    return dates.reduce(into: WeekdayAbbreviations) {
       let weekday = Calendar.autoupdatingCurrent.component(.weekday, from: $1)
       let pair = $0[weekday] ?? (WeekdayChartFormat.format($1), 0)
       $0[weekday] = (pair.0, pair.1 + 1)
