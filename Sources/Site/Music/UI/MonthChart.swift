af60001c3cbcf88c398f04b7ac04d724e729f89d
Greg Bolsinga <bolsinga@mac.com>
2023-04-30 19:36:32 -0700

    Pass [Date] to MonthChart and WeekdayChart

--- Sources/Site/Music/UI/MonthChart.swift
+++ Sources/Site/Music/UI/MonthChart.swift
@@ -9,22 +9,20 @@ import Charts
 import SwiftUI
 
 struct MonthChart: View {
-  let shows: [Show]
+  let dates: [Date]
 
-  private var computeMonthShowCounts: [Int: (String, Int)] {  // month as int: (month as string, count of shows for that month)
+  private var computeMonthCounts: [Int: (String, Int)] {  // month as int: (month as string, count for that month)
     let format = Date.FormatStyle.dateTime.month(.abbreviated)
-    return shows.filter { !$0.date.isUnknown }.reduce(into: [Int: (String, Int)]()) {
-      if let date = $1.date.date {
-        let month = Calendar.autoupdatingCurrent.component(.month, from: date)
-        let pair = $0[month] ?? (format.format(date), 0)
-        $0[month] = (pair.0, pair.1 + 1)
-      }
+    return dates.reduce(into: [Int: (String, Int)]()) {
+      let month = Calendar.autoupdatingCurrent.component(.month, from: $1)
+      let pair = $0[month] ?? (format.format($1), 0)
+      $0[month] = (pair.0, pair.1 + 1)
     }
   }
 
   var body: some View {
-    let monthShowCounts = computeMonthShowCounts.sorted { $0.key < $1.key }  // array of dictionary elements
-    Chart(monthShowCounts, id: \.key) { item in
+    let monthCounts = computeMonthCounts.sorted { $0.key < $1.key }  // array of dictionary elements
+    Chart(monthCounts, id: \.key) { item in
       BarMark(
         x: .value(
           Text(
@@ -49,6 +47,6 @@ struct MonthChart: View {
 
 struct MonthChart_Previews: PreviewProvider {
   static var previews: some View {
-    MonthChart(shows: [])
+    MonthChart(dates: [])
   }
 }
