a5069eb31ce96f1ca56f35d2ea18deaa0e55e332
Greg Bolsinga <bolsinga@mac.com>
2023-05-26 13:58:38 -0700

    forgot to format again

--- Sources/Site/Music/PartialDate+Span.swift
+++ Sources/Site/Music/PartialDate+Span.swift
@@ -20,9 +20,9 @@ extension Sequence<PartialDate> {
       let comps = Calendar.autoupdatingCurrent.dateComponents(
         [.year, .month, .day], from: min, to: max)
       if let year = comps.year, let month = comps.month, let day = comps.day {
-        guard year > 0 else { return 1 } // It's less than a year difference, so the sequence spans 1 year.
-        guard year == 1 else { return year } // It's more than 1 year, so just count the years, no rounding. Matches RelativeDateTimeFormatter() output.
-        return year + (month > 0 ? 1 : (day > 0 ? 1 : 0)) // It's 1 year and some time.
+        guard year > 0 else { return 1 }  // It's less than a year difference, so the sequence spans 1 year.
+        guard year == 1 else { return year }  // It's more than 1 year, so just count the years, no rounding. Matches RelativeDateTimeFormatter() output.
+        return year + (month > 0 ? 1 : (day > 0 ? 1 : 0))  // It's 1 year and some time.
       }
     }
 
