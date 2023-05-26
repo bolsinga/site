17c1caec620f5b96d56504731925d942ce097913
Greg Bolsinga <bolsinga@mac.com>
2023-05-26 12:58:01 -0700

    Try to match RelativeDateFormatter behavior for year spans > 1
    tweaks for 1 year and change, as well as less than 1 year.

--- Sources/Site/Music/PartialDate+Span.swift
+++ Sources/Site/Music/PartialDate+Span.swift
@@ -17,9 +17,12 @@ extension Sequence<PartialDate> {
     guard knownDates.count > 1 else { return 1 }
 
     if let max = knownDates.max(), let min = knownDates.min() {
-      let comps = Calendar.autoupdatingCurrent.dateComponents([.year], from: min, to: max)
-      if let year = comps.year {
-        return year
+      let comps = Calendar.autoupdatingCurrent.dateComponents(
+        [.year, .month, .day], from: min, to: max)
+      if let year = comps.year, let month = comps.month, let day = comps.day {
+        guard year > 0 else { return 1 } // It's less than a year difference, so the sequence spans 1 year.
+        guard year == 1 else { return year } // It's more than 1 year, so just count the years, no rounding. Matches RelativeDateTimeFormatter() output.
+        return year + (month > 0 ? 1 : (day > 0 ? 1 : 0)) // It's 1 year and some time.
       }
     }
 
