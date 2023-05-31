9b3836aafe3febc62d273e708137392fb2ed4aab
Greg Bolsinga <bolsinga@mac.com>
2023-05-31 10:42:44 -0700

    Add Locatable protocol (#348)
    - It has an extension to calculate the MKCoordinateRegion.
    - This is necessary since it is not possible (?!?) to create a hand
    rolled CLPlacemark for tests that has a center and a region.
    - Make CLPlacemark conform to Locatable, and have LocationMap use the
    Locatable interface

---
+++ Sources/Site/Utility/Locatable.swift
@@ -0,0 +1,22 @@
+//
+//  Locatable.swift
+//
+//
+//  Created by Greg Bolsinga on 5/31/23.
+//
+
+import CoreLocation
+import MapKit
+
+protocol Locatable {
+  var center: CLLocationCoordinate2D { get }
+  var radius: CLLocationDistance { get }
+}
+
+extension Locatable {
+  var region: MKCoordinateRegion {
+    let radius = radius
+    return MKCoordinateRegion(
+      center: self.center, latitudinalMeters: radius, longitudinalMeters: radius)
+  }
+}
