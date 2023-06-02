196bd0a55bb6e5bff3a0bdb1ba58bec637999be8
Greg Bolsinga <bolsinga@mac.com>
2023-06-01 18:27:20 -0700

    Gate geocoding withing Atlas, so all client can take advantage

--- Sources/Site/Music/Atlas.swift
+++ Sources/Site/Music/Atlas.swift
@@ -9,17 +9,57 @@ import CoreLocation
 import Foundation
 
 actor Atlas {
+  private enum Constants {
+    static let maxRequests = 50
+    static let timeUntilReset = Duration.seconds(60)
+  }
+
   typealias Cache = [Location: CLPlacemark]
 
   private var cache: Cache = [:]
+
+  private var count = 0
+  private var waitUntil: ContinuousClock.Instant = .now + Constants.timeUntilReset
+
+  private func idleAndReset() async throws {
+    try await ContinuousClock().sleep(until: waitUntil)
+    waitUntil = .now + Constants.timeUntilReset
+    count = 0
+  }
 
   public func geocode(_ location: Location) async throws -> CLPlacemark {
     if let result = self[location] {
       return result
     }
-    let result: CLPlacemark = try await location.geocode()
+    let result = try await gatedGeocode(location)
     self[location] = result
     return result
+  }
+
+  private func gatedGeocode(_ location: Location) async throws -> CLPlacemark {
+    if count != 0, count % Constants.maxRequests == 0 {
+      // hit max requests
+      try await idleAndReset()
+    }
+
+    var retry = false
+    repeat {
+      do {
+        let placemark = try await location.geocode()
+        count += 1
+        return placemark
+      } catch let error as NSError {
+        if error.code == CLError.network.rawValue, error.domain == kCLErrorDomain {
+          // throttling error
+          try await idleAndReset()
+          retry = true
+        } else {
+          throw error
+        }
+      } catch {
+        throw error
+      }
+    } while retry
   }
 
   subscript(index: Location) -> CLPlacemark? {
--- Sources/Site/Music/BatchGeocode.swift
+++ Sources/Site/Music/BatchGeocode.swift
@@ -8,11 +8,6 @@
 import CoreLocation
 import Foundation
 
-private enum Constants {
-  static let maxRequests = 50
-  static let timeUntilReset = Duration.seconds(60)
-}
-
 struct BatchGeocode: AsyncSequence {
   typealias Element = (Location, CLPlacemark)
 
@@ -24,44 +19,16 @@ struct BatchGeocode: AsyncSequence {
     let locations: [Location]
 
     var index: Int = 0
-    var lastWaitingIndex: Int = 0
-    var waitUntil: ContinuousClock.Instant = .now + Constants.timeUntilReset
-
-    private mutating func idleAndReset() async throws {
-      try await ContinuousClock().sleep(until: waitUntil)
-      waitUntil = .now + Constants.timeUntilReset
-      lastWaitingIndex = index
-    }
 
     mutating func next() async throws -> Element? {
       guard !Task.isCancelled else { return nil }
 
       guard index < locations.count else { return nil }
 
-      let requestIndex = abs(index - lastWaitingIndex)
-      if requestIndex != 0, requestIndex % Constants.maxRequests == 0 {
-        // hit max requests
-        try await idleAndReset()
-      }
-
-      var retry: Bool = true
-      while retry {
-        do {
-          let location = locations[index]
-          let placemark = try await atlas.geocode(location)
-          index += 1
-          return (location, placemark)
-        } catch let error as NSError {
-          if error.code == CLError.network.rawValue, error.domain == kCLErrorDomain {
-            // throttling error
-            try await idleAndReset()
-          }
-        } catch {
-          retry = false
-        }
-      }
-
-      return nil
+      let location = locations[index]
+      let placemark = try await atlas.geocode(location)
+      index += 1
+      return (location, placemark)
     }
   }
 
