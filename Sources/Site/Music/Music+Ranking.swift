ff8ede6f9945e83bc18e4aa39b10cf6f52f29e9d
Greg Bolsinga <bolsinga@mac.com>
2023-06-06 09:15:21 -0700

    Make rank sorting a closure

--- Sources/Site/Music/Music+Ranking.swift
+++ Sources/Site/Music/Music+Ranking.swift
@@ -56,7 +56,9 @@ extension Music {
     return computeRankings(items: venueArtistCounts)
   }
 
-  internal func computeRankings<T, V, R>(items: [(T, V)], rankBuilder: (Int, V) -> R) -> [T: R]
+  internal func computeRankings<T, V, R>(
+    items: [(T, V)], rankBuilder: (Int, V) -> R, rankSorted: (V, V) -> Bool
+  ) -> [T: R]
   where V: Hashable, V: Comparable {
     let itemRanks: [V: [T]] = Dictionary(grouping: items) { $0.1 }
       .reduce(into: [:]) {
@@ -65,13 +67,12 @@ extension Music {
         $0[$1.key] = arr
       }
 
-    // ordered ascending
-    let itemsOrdered: [([T], V)] = itemRanks.sorted(by: { $0.key < $1.key })
+    let itemsOrdered: [([T], V)] = itemRanks.sorted(by: { rankSorted($0.key, $1.key) })
       .reduce(into: []) { $0.append(($1.value, $1.key)) }
 
     var rank = 1
     // T : Ordinal rank (1, 2, 3 etc)
-    let itemRankMap: [T: R] = itemsOrdered.reversed().reduce(into: [:]) {
+    let itemRankMap: [T: R] = itemsOrdered.reduce(into: [:]) {
       dictionary, itemRankings in
       itemRankings.0.forEach { item in
         dictionary[item] = rankBuilder(rank, itemRankings.1)
@@ -84,6 +85,8 @@ extension Music {
   internal func computeRankings<T>(items: [(T, Int)]) -> [T: Ranking] {
     computeRankings(items: items) {
       Ranking(rank: $0, value: $1)
+    } rankSorted: { lhs, rhs in
+      return lhs > rhs
     }
   }
 }
