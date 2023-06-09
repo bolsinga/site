83df5242794cb24fcfe3f98858d3bab5fe94e2e0
Greg Bolsinga <bolsinga@mac.com>
2023-06-09 09:45:22 -0700

    Add Annum to the musicDestinations ViewModifier

--- Sources/Site/Music/UI/MusicDestinationModifier.swift
+++ Sources/Site/Music/UI/MusicDestinationModifier.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct MusicDestinationModifier: ViewModifier {
+  @Environment(\.vault) private var vault: Vault
+
   func body(content: Content) -> some View {
     content
       .navigationDestination(for: Show.self) { ShowDetail(show: $0) }
       .navigationDestination(for: Venue.self) { VenueDetail(venue: $0) }
       .navigationDestination(for: Artist.self) { ArtistDetail(artist: $0) }
+      .navigationDestination(for: Annum.self) { annum in
+        YearDetail(shows: vault.lookup.decadesMap[annum.decade]?[annum] ?? [], annum: annum)
+      }
   }
 }
 
