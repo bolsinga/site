76bf9533d14b48dea00ea38e18ba711303a0d2ee
Greg Bolsinga <bolsinga@mac.com>
2023-06-10 11:52:47 -0700

    Add Annum ParseableFormatStyle (#397)
    - This will allow them to be written / read from JSON strings.

---
+++ Sources/Site/Music/ParseableFormatStyle+Annum.swift
@@ -0,0 +1,59 @@
+//
+//  ParseableFormatStyle+Annum.swift
+//
+//
+//  Created by Greg Bolsinga on 6/10/23.
+//
+
+import Foundation
+
+extension Annum.FormatStyle {
+  public struct ParseStrategy: Foundation.ParseStrategy {
+    enum ValidationError: Error {
+      case invalidCharacters
+    }
+
+    static let validCharacterSet = CharacterSet(charactersIn: "0123456789")
+
+    public init() {}
+
+    public func parse(_ value: String) throws -> Annum {
+      let trimmedValue = value.trimmingCharacters(in: .whitespaces)
+
+      if trimmedValue == Annum.unknown.formatted() {
+        return .unknown
+      }
+
+      guard trimmedValue.rangeOfCharacter(from: Self.validCharacterSet.inverted) == nil else {
+        throw ValidationError.invalidCharacters
+      }
+
+      if let validAnnum = Int(trimmedValue) {
+        return .year(validAnnum)
+      }
+
+      return .unknown
+    }
+  }
+}
+
+extension Annum.FormatStyle: ParseableFormatStyle {
+  public var parseStrategy: Annum.FormatStyle.ParseStrategy {
+    .init()
+  }
+}
+
+extension Annum {
+  init(_ string: String) throws {
+    self = try Annum.FormatStyle().parseStrategy.parse(string)
+  }
+
+  init<T, Value>(_ value: Value, standard: T) throws
+  where T: ParseStrategy, Value: StringProtocol, T.ParseInput == String, T.ParseOutput == Annum {
+    self = try standard.parse(value.description)
+  }
+}
+
+extension ParseableFormatStyle where Self == Annum.FormatStyle {
+  public static var annum: Self { .init() }
+}
