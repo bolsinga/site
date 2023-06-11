87f294ab44f461259bbd8f6fb34f552c1e3907e2
Greg Bolsinga <bolsinga@mac.com>
2023-06-11 15:39:17 -0700

    Add a tool to generate associated domains json (#407)
    - Unfortunately the format is not JSONEncodable since "appIDs" is an
    array of strings and "components" is an array of dictionaries.

---
+++ Sources/site_associated_domains/Program.swift
@@ -0,0 +1,48 @@
+//
+//  Program.swift
+//  site
+//
+//  Created by Greg Bolsinga on 6/11/23.
+//
+
+import ArgumentParser
+import Foundation
+
+@main
+struct Program: AsyncParsableCommand {
+  @Argument(help: "Application Identifier (from developer.apple.com)")
+  var applicationIdentifier: String
+
+  @Argument(help: "Bundle Identifier")
+  var bundleIdentifier: String
+
+  func run() async throws {
+
+    let contents = """
+      {
+        "appLinks" : {
+          "details" : [
+            {
+              "appIDs" : [ "\(applicationIdentifier).\(bundleIdentifier)" ],
+              "components" : [
+                 {
+                    "/": "/bands/*",
+                    "comment": "Allows artists."
+                 },
+                 {
+                    "/": "/dates/*",
+                    "comment": "Allows shows."
+                 },
+                 {
+                    "/": "/venues/*",
+                    "comment": "Allows venues."
+                 }
+              ]
+            }
+          ]
+        }
+      }
+      """
+    print(contents)
+  }
+}
