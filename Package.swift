adfaa087a78ebb371455820d57e186c7212d1714
Greg Bolsinga <bolsinga@mac.com>
2023-06-11 14:27:02 -0700

    Add a tool to generate associated domains json
    - Unfortunately the format is not JSONEncodable since "appIDs" is an array of strings and "components" is an array of dictionaries.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
   products: [
     .library(name: "Site", targets: ["Site"]),
     .executable(name: "site_tool", targets: ["site_tool"]),
+    .executable(name: "site_associated_domains", targets: ["site_associated_domains"]),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
@@ -23,6 +24,11 @@ let package = Package(
       name: "site_tool",
       dependencies: [
         .byName(name: "Site"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]),
+    .executableTarget(
+      name: "site_associated_domains",
+      dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
       ]),
   ]
