ce3b82e34ed47edcb0249397760ccff55a6f36ec
Greg Bolsinga <bolsinga@mac.com>
2023-06-13 14:19:02 -0700

    Encapsulate model update behavior within the class (#416)

--- Sources/Site/Music/UI/ArchiveNavigation.swift
+++ Sources/Site/Music/UI/ArchiveNavigation.swift
@@ -6,8 +6,39 @@
 //
 
 import Combine
+import Foundation
 
 final class ArchiveNavigation: ObservableObject {
   @Published var selectedCategory: ArchiveCategory?
   @Published var navigationPath: [ArchivePath] = []
+
+  private var pendingNavigationPath: [ArchivePath]?
+
+  func restoreNavigation(selectionData: Data?, pathData: Data?) {
+    if let data = selectionData {
+      if let data = pathData {
+        // Hold onto the loading navigationPath for after the selectedCategory changes.
+        var pending = [ArchivePath]()
+        pending.jsonData = data
+        pendingNavigationPath = pending
+      }
+
+      // Changing the selectedCategory will reset the NavigationStack's navigationPath.
+      if selectedCategory != nil {
+        selectedCategory?.jsonData = data
+      } else {
+        var category = ArchiveCategory.today
+        category.jsonData = data
+        selectedCategory = category
+      }
+    }
+  }
+
+  func restorePendingData() {
+    // Change the navigationPath after selectedCategory changes.
+    if let pendingNavigationPath {
+      navigationPath = pendingNavigationPath
+      self.pendingNavigationPath = nil
+    }
+  }
 }
