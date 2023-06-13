40d63b8530b0f52427de89dedd8626d734ff605e
Greg Bolsinga <bolsinga@mac.com>
2023-06-13 14:09:12 -0700

    Encapsulate model update behavior within the class

--- Sources/Site/Music/UI/ArchiveStorageModifier.swift
+++ Sources/Site/Music/UI/ArchiveStorageModifier.swift
@@ -12,37 +12,17 @@ struct ArchiveStorageModifier: ViewModifier {
 
   @SceneStorage("selected.category") private var selectedCategoryData: Data?
   @SceneStorage("navigation.path") private var navigationPathData: Data?
-  @State private var pendingNavigationPath: [ArchivePath]?
 
   func body(content: Content) -> some View {
     content
       .task {
-        if let data = selectedCategoryData {
-          if let data = navigationPathData {
-            // Hold onto the loading navigationPath for after the selectedCategory changes.
-            var pending = [ArchivePath]()
-            pending.jsonData = data
-            pendingNavigationPath = pending
-          }
-
-          // Changing the selectedCategory will reset the NavigationStack's navigationPath.
-          if archiveNavigation.selectedCategory != nil {
-            archiveNavigation.selectedCategory?.jsonData = data
-          } else {
-            var category = ArchiveCategory.today
-            category.jsonData = data
-            archiveNavigation.selectedCategory = category
-          }
-        }
+        archiveNavigation.restoreNavigation(
+          selectionData: selectedCategoryData, pathData: navigationPathData)
       }
       .onChange(of: archiveNavigation.selectedCategory) { newValue in
         selectedCategoryData = newValue?.jsonData
 
-        // Change the navigationPath after selectedCategory changes.
-        if let pendingNavigationPath {
-          archiveNavigation.navigationPath = pendingNavigationPath
-          self.pendingNavigationPath = nil
-        }
+        archiveNavigation.restorePendingData()
       }
       .onChange(of: archiveNavigation.navigationPath) { newPath in
         navigationPathData = newPath.jsonData
