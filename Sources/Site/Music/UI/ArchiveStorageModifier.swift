b42ae87a1be4576626935102c589f3571a02d0d7
Greg Bolsinga <bolsinga@mac.com>
2023-06-21 13:57:27 -0700

    ArchiveCategory enum is a String (#424)
    - Simplifies storage for sure!

--- Sources/Site/Music/UI/ArchiveStorageModifier.swift
+++ Sources/Site/Music/UI/ArchiveStorageModifier.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ArchiveStorageModifier: ViewModifier {
   let archiveNavigation: ArchiveNavigation
 
-  @SceneStorage("selected.category") private var selectedCategoryData: Data?
+  @SceneStorage("selected.category") private var selectedCategoryStorage: ArchiveCategory?
   @SceneStorage("navigation.path") private var navigationPathData: Data?
 
   func body(content: Content) -> some View {
     content
       .task {
         archiveNavigation.restoreNavigation(
-          selectionData: selectedCategoryData, pathData: navigationPathData)
+          selectedCategoryStorage: selectedCategoryStorage, pathData: navigationPathData)
       }
       .onChange(of: archiveNavigation.selectedCategory) { newValue in
-        selectedCategoryData = newValue?.jsonData
+        selectedCategoryStorage = newValue
 
         archiveNavigation.restorePendingData()
       }
