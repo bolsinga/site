d5348eb579e84e55029403803255b8a3d7bcbd6f
Greg Bolsinga <bolsinga@mac.com>
2023-06-21 11:18:43 -0700

    Add Logger statements for debugging
    This is the start! Fixes #422

--- Sources/Site/Music/Vault+URL.swift
+++ Sources/Site/Music/Vault+URL.swift
@@ -6,9 +6,18 @@
 //
 
 import Foundation
+import os
+
+extension Logger {
+  static let vault = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "vault")
+}
 
 extension Vault {
   public static func load(url: URL) async throws -> Vault {
+    Logger.vault.log("start")
+    defer {
+      Logger.vault.log("end")
+    }
     let music = try await Music.load(url: url)
     return await Vault.create(music: music)
   }
