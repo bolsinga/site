6940e1c8968919eb2416717ce5e5e503c932b344
Greg Bolsinga <bolsinga@mac.com>
2023-06-21 14:37:30 -0700

    Add Spotlight search for Artist, Venue, Annum
    Fixes #241

--- Sources/Site/Music/Annum+NSUserActivity.swift
+++ Sources/Site/Music/Annum+NSUserActivity.swift
@@ -5,11 +5,21 @@
 //  Created by Greg Bolsinga on 6/21/23.
 //
 
+import CoreSpotlight
 import Foundation
 
 extension Annum: PathRestorableUserActivity {
   func updateActivity(_ userActivity: NSUserActivity) {
     userActivity.isEligibleForHandoff = true
+
+    userActivity.isEligibleForSearch = true
+    userActivity.title = self.formatted()
+    let attributes = CSSearchableItemAttributeSet(contentType: .content)
+    attributes.contentDescription = String(
+      localized: "See Shows From This Year", bundle: .module,
+      comment: "Spotlight Description for Annum")
+    userActivity.contentAttributeSet = attributes
+
     try? userActivity.setTypedPayload(self.archivePath)
   }
 }
