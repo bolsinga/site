99aef9cda2a5920e6c29a5cd262a86601afd417c
Greg Bolsinga <bolsinga@mac.com>
2023-06-22 15:10:20 -0700

    Add Spotlight search for Artist, Venue, Annum (#434)
    Fixes #241

--- Sources/Site/Music/Artist+NSUserActivity.swift
+++ Sources/Site/Music/Artist+NSUserActivity.swift
@@ -5,11 +5,21 @@
 //  Created by Greg Bolsinga on 6/21/23.
 //
 
+import CoreSpotlight
 import Foundation
 
 extension Artist: PathRestorableUserActivity {
   func updateActivity(_ userActivity: NSUserActivity) {
     userActivity.isEligibleForHandoff = true
+
+    userActivity.isEligibleForSearch = true
+    userActivity.title = self.name
+    let attributes = CSSearchableItemAttributeSet(contentType: .content)
+    attributes.contentDescription = String(
+      localized: "See Shows With This Artist", bundle: .module,
+      comment: "Spotlight Description for Artist")
+    userActivity.contentAttributeSet = attributes
+
     try? userActivity.setTypedPayload(self.archivePath)
   }
 }
