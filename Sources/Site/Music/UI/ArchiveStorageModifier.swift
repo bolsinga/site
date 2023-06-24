bolsinga/site

commit b42ae87a1be4576626935102c589f3571a02d0d7

ArchiveCategory enum is a String (#424)
- Simplifies storage for sure!

Sources/Site/Music/UI/ArchiveStorageModifier.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ArchiveStorageModifier: ViewModifier {
   let archiveNavigation: ArchiveNavigation
 
-  @SceneStorage("selected.category") private var selectedCategoryData: Data?
+  @SceneStorage("selected.category") private var selectedCategoryStorage: ArchiveCategory?
   @SceneStorage("navigation.path") private var navigationPathData: Data?
 
   func body(content: Content) -> some View {
     content
       .task {
         archiveNavigation.restoreNavigation(
-          selectionData: selectedCategoryData, pathData: navigationPathData)
+          selectedCategoryStorage: selectedCategoryStorage, pathData: navigationPathData)
       }
       .onChange(of: archiveNavigation.selectedCategory) { newValue in
-        selectedCategoryData = newValue?.jsonData
+        selectedCategoryStorage = newValue
 
         archiveNavigation.restorePendingData()
       }

Sources/Site/Music/UI/ArchiveCategory.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum ArchiveCategory: CaseIterable, Codable {
+enum ArchiveCategory: String, CaseIterable {
   case today
   case stats
   case shows
@@ -41,18 +41,6 @@ enum ArchiveCategory: CaseIterable, Codable {
       Label(self.localizedString, systemImage: "music.note.house")
     case .artists:
       Label(self.localizedString, systemImage: "music.mic")
-    }
-  }
-
-  var jsonData: Data? {
-    get {
-      try? JSONEncoder().encode(self)
-    }
-    set {
-      guard let data = newValue,
-        let category = try? JSONDecoder().decode(Self.self, from: data)
-      else { return }
-      self = category
     }
   }
 }

Sources/Site/Music/UI/ArchiveNavigation.swift
@@ -14,8 +14,8 @@ final class ArchiveNavigation: ObservableObject {
 
   private var pendingNavigationPath: [ArchivePath]?
 
-  func restoreNavigation(selectionData: Data?, pathData: Data?) {
-    if let selectionData {
+  func restoreNavigation(selectedCategoryStorage: ArchiveCategory?, pathData: Data?) {
+    if let selectedCategoryStorage {
       if let pathData {
         // Hold onto the loading navigationPath for after the selectedCategory changes.
         var pending = [ArchivePath]()
@@ -24,13 +24,7 @@ final class ArchiveNavigation: ObservableObject {
       }
 
       // Changing the selectedCategory will reset the NavigationStack's navigationPath.
-      if selectedCategory != nil {
-        selectedCategory?.jsonData = selectionData
-      } else {
-        var category = ArchiveCategory.today
-        category.jsonData = selectionData
-        selectedCategory = category
-      }
+      selectedCategory = selectedCategoryStorage
     } else {
       if let pathData {
         navigationPath.jsonData = pathData

commit 0f379997a75dddd2d7dd22d8389128db99e70814

Store selectedArchiveCategory as String?
Not sure why but it doesn't work if the enum (that is a String) is saved. Broken by #424

Sources/Site/Music/UI/ArchiveStorageModifier.swift
@@ -15,7 +15,7 @@ extension Logger {
 struct ArchiveStorageModifier: ViewModifier {
   let archiveNavigation: ArchiveNavigation
 
-  @SceneStorage("selected.category") private var selectedCategoryStorage: ArchiveCategory?
+  @SceneStorage("selected.category") private var selectedCategoryStorage: String?
   @SceneStorage("navigation.path") private var navigationPathData: Data?
 
   func body(content: Content) -> some View {
@@ -25,12 +25,14 @@ struct ArchiveStorageModifier: ViewModifier {
         defer {
           Logger.archive.log("end restore")
         }
+        let archiveCategory =
+          selectedCategoryStorage != nil ? ArchiveCategory(rawValue: selectedCategoryStorage!) : nil
         archiveNavigation.restoreNavigation(
-          selectedCategoryStorage: selectedCategoryStorage, pathData: navigationPathData)
+          selectedCategoryStorage: archiveCategory, pathData: navigationPathData)
       }
       .onChange(of: archiveNavigation.selectedCategory) { newValue in
         Logger.archive.log("category: \(newValue?.rawValue ?? "nil", privacy: .public)")
-        selectedCategoryStorage = newValue
+        selectedCategoryStorage = newValue?.rawValue ?? nil
 
         archiveNavigation.restorePendingData()
       }