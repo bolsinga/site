5c1a31169f281d090e7bf72b18bd6dcbd6ea6df1
Greg Bolsinga <bolsinga@mac.com>
2023-07-12 09:35:15 -0700

    Add VaultModel (#472)
    - Encapsulates the small logic for this code. 
    - Allows it to be used by Commands, which need to be defined up for the
    Scene, which is not in this SPM.

---
+++ Sources/Site/Music/VaultModel.swift
@@ -0,0 +1,38 @@
+//
+//  VaultModel.swift
+//
+//
+//  Created by Greg Bolsinga on 7/12/23.
+//
+
+import Combine
+import Foundation
+import os
+
+extension Logger {
+  static let vaultModel = Logger(category: "vaultModel")
+}
+
+public final class VaultModel: ObservableObject {
+  let url: URL
+
+  @Published var vault: Vault?
+  @Published var error: Error?
+
+  public init(url: URL) {
+    self.url = url
+  }
+
+  func load() async {
+    Logger.vaultModel.log("start")
+    defer {
+      Logger.vaultModel.log("end")
+    }
+    do {
+      vault = try await Vault.load(url: url)
+    } catch {
+      Logger.vaultModel.log("error: \(error.localizedDescription, privacy: .public)")
+      self.error = error
+    }
+  }
+}
