553dcc6313d86e7ee149c2b78bfed61f5fe37ba8
Greg Bolsinga <bolsinga@mac.com>
2023-08-01 19:16:04 -0700

    Add ShareLink for shows
    - Right now only Shows are shareable (since their IDs are most stable).
    - Fixes #476

---
+++ Sources/Site/Music/PathRestorableShareable.swift
@@ -0,0 +1,23 @@
+//
+//  PathRestorableShareable.swift
+//
+//
+//  Created by Greg Bolsinga on 8/2/23.
+//
+
+import SwiftUI
+
+protocol PathRestorableShareable: PathRestorable {
+  func subject(vault: Vault) -> Text
+  func message(vault: Vault) -> Text
+}
+
+extension Show: PathRestorableShareable {
+  func subject(vault: Vault) -> Text {
+    Text(self.formatted(.headlinerAndVenue, lookup: vault.lookup))
+  }
+
+  func message(vault: Vault) -> Text {
+    Text(self.formatted(.full, lookup: vault.lookup))
+  }
+}
