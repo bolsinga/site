bolsinga/site

commit 1b018d45f3ff5d2d09651ff4b4323664f52859ed

Today Shows are Published by the VaultModel (#488)
- This gets it out of the UI code path. This will make it more useful
(perhaps?) in a Widget context.

Sources/Site/Music/UI/VaultView.swift
@@ -22,7 +22,7 @@ public struct VaultView: View {
   public var body: some View {
     Group {
       if let vault = model.vault {
-        ArchiveCategorySplit(vault: vault)
+        ArchiveCategorySplit(vault: vault, model: model)
           .refreshable {
             Logger.vaultLoad.log("refresh")
             await model.load()

Sources/Site/Music/VaultModel.swift
@@ -18,6 +18,7 @@ extension Logger {
 
   @Published var vault: Vault?
   @Published var error: Error?
+  @Published var todayShows: [Show] = []
 
   public init(url: URL) {
     self.url = url
@@ -30,9 +31,30 @@ extension Logger {
     }
     do {
       vault = try await Vault.load(url: url)
+      updateTodayShows()
+      await monitorDayChanges()
     } catch {
       Logger.vaultModel.log("error: \(error.localizedDescription, privacy: .public)")
       self.error = error
+    }
+  }
+
+  private func updateTodayShows() {
+    guard let vault else {
+      Logger.vaultModel.log("No Vault to calculate todayShows.")
+      return
+    }
+
+    todayShows = vault.music.showsOnDate(Date.now).sorted {
+      vault.comparator.showCompare(lhs: $0, rhs: $1, lookup: vault.lookup)
+    }
+
+    Logger.vaultModel.log("Today Count: \(self.todayShows.count, privacy: .public)")
+  }
+
+  private func monitorDayChanges() async {
+    for await _ in NotificationCenter.default.notifications(named: .NSCalendarDayChanged) {
+      updateTodayShows()
     }
   }
 }

Sources/Site/Music/UI/ArchiveCategorySplit.swift
@@ -9,17 +9,15 @@ import SwiftUI
 import os
 
 extension Logger {
-  static let today = Logger(category: "today")
   static let link = Logger(category: "link")
 }
 
 struct ArchiveCategorySplit: View {
   let vault: Vault
+  @ObservedObject var model: VaultModel
 
   @SceneStorage("venue.sort") private var venueSort = VenueSort.alphabetical
   @SceneStorage("artist.sort") private var artistSort = ArtistSort.alphabetical
-
-  @State private var todayShows: [Show] = []
 
   @StateObject private var archiveNavigation = ArchiveNavigation()
 
@@ -33,7 +31,7 @@ struct ArchiveCategorySplit: View {
       LabeledContent {
         switch category {
         case .today:
-          Text(todayShows.count.formatted(.number))
+          Text(model.todayShows.count.formatted(.number))
             .animation(.easeInOut)
         case .stats:
           EmptyView()
@@ -58,19 +56,13 @@ struct ArchiveCategorySplit: View {
     } detail: {
       NavigationStack(path: $archiveNavigation.navigationPath) {
         ArchiveCategoryDetail(
-          category: archiveNavigation.selectedCategory, todayShows: $todayShows,
+          category: archiveNavigation.selectedCategory, todayShows: $model.todayShows,
           venueSort: $venueSort,
           artistSort: $artistSort)
       }
     }
     .archiveStorage(archiveNavigation: archiveNavigation)
     .environment(\.vault, vault)
-    .onDayChanged {
-      self.todayShows = vault.music.showsOnDate(Date.now).sorted {
-        vault.comparator.showCompare(lhs: $0, rhs: $1, lookup: vault.lookup)
-      }
-      Logger.today.log("Count: \(todayShows.count, privacy: .public)")
-    }
     .onContinueUserActivity(ArchivePath.activityType) { userActivity in
       do {
         archiveNavigation.navigate(to: try userActivity.archivePath())

Sources/Site/Music/UI/DayChangedModifer.swift
@@ -1,16 +1,0 @@
-//
-//  DayChangedModifer.swift
-//
-//
-//  Created by Greg Bolsinga on 5/17/23.
-//
-
-import SwiftUI
-
-extension View {
-  func onDayChanged(
-    action: @escaping () -> Void
-  ) -> some View {
-    self.onNotification(name: .NSCalendarDayChanged, action: action)
-  }
-}

commit 62ed2116f08e6e4ae95026688b69ad18f4e93a24

monitorDayChanges in a separate Task
- This way refreshable returns and the UI will stop showing a refresh
- Add more logging to track this stuff.
- Caused by #488

Sources/Site/Music/UI/VaultView.swift
@@ -24,7 +24,10 @@ public struct VaultView: View {
       if let vault = model.vault {
         ArchiveCategorySplit(vault: vault, model: model)
           .refreshable {
-            Logger.vaultLoad.log("refresh")
+            Logger.vaultLoad.log("start refresh")
+            defer {
+              Logger.vaultLoad.log("end refresh")
+            }
             await model.load()
           }
       } else if let error = model.error {
@@ -33,7 +36,10 @@ public struct VaultView: View {
         ProgressView()
       }
     }.task {
-      Logger.vaultLoad.log("task")
+      Logger.vaultLoad.log("start task")
+      defer {
+        Logger.vaultLoad.log("end task")
+      }
       await model.load()
     }
   }

Sources/Site/Music/VaultModel.swift
@@ -32,7 +32,9 @@ extension Logger {
     do {
       vault = try await Vault.load(url: url)
       updateTodayShows()
-      await monitorDayChanges()
+      Task {
+        await monitorDayChanges()
+      }
     } catch {
       Logger.vaultModel.log("error: \(error.localizedDescription, privacy: .public)")
       self.error = error
@@ -53,7 +55,12 @@ extension Logger {
   }
 
   private func monitorDayChanges() async {
+    Logger.vaultModel.log("start day monitoring")
+    defer {
+      Logger.vaultModel.log("end day monitoring")
+    }
     for await _ in NotificationCenter.default.notifications(named: .NSCalendarDayChanged) {
+      Logger.vaultModel.log("day changed")
       updateTodayShows()
     }
   }