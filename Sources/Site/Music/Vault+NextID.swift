bolsinga/site

commit 353f6135358754ba38159d46b44db4a7b2e7aa74

Move next ID funcs to library (#547)

Sources/Site/Music/Vault+NextID.swift
@@ -9,18 +9,18 @@ import Foundation
 import Site
 
 extension Vault {
-  var nextShowID: Show.ID {
-    let nextIndex = max(music.shows.count, 0)
+  public var nextShowID: Show.ID {
+    let nextIndex = max(concerts.count, 0)
     return "\(ArchivePath.showPrefix)\(nextIndex)"
   }
 
-  var nextVenueID: Venue.ID {
-    let nextIndex = max(music.venues.count, 0)
+  public var nextVenueID: Venue.ID {
+    let nextIndex = max(venues.count, 0)
     return "\(ArchivePath.venuePrefix)\(nextIndex)"
   }
 
-  var nextArtistID: Venue.ID {
-    let nextIndex = max(music.artists.count, 0)
+  public var nextArtistID: Venue.ID {
+    let nextIndex = max(artists.count, 0)
     return "\(ArchivePath.artistPrefix)\(nextIndex)"
   }
 }

commit df4b6f8b1e6817e738b6f01e829717dd4aec8f89

remove import
caused by #547

Sources/Site/Music/Vault+NextID.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Site
 
 extension Vault {
   public var nextShowID: Show.ID {