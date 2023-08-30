bd27092b72f91364436633842d848ed1412bfd26
Greg Bolsinga <bolsinga@mac.com>
2023-08-29 17:29:59 -0700

    Add Location.FormatStyle

---
+++ Sources/Site/Music/Location+FormatStyle.swift
@@ -0,0 +1,64 @@
+//
+//  Location+FormatStyle.swift
+//
+//
+//  Created by Greg Bolsinga on 8/29/23.
+//
+
+import Foundation
+
+extension Location {
+  public struct FormatStyle: Codable, Equatable, Hashable {
+    public enum Style: Codable, Equatable, Hashable {
+      case oneLine
+    }
+
+    let style: Style
+
+    public init(_ style: Style = .oneLine) {
+      self.style = style
+    }
+
+    public func style(_ style: Style) -> Self {
+      .init(style)
+    }
+  }
+}
+
+extension Location.FormatStyle: Foundation.FormatStyle {
+  public func format(_ value: Location) -> String {
+    switch style {
+    case .oneLine:
+      var physicalAddress: String
+      if let street = value.street {
+        physicalAddress = "\(street), \(value.city), \(value.state)"
+      } else {
+        physicalAddress = "\(value.city), \(value.state))"
+      }
+
+      if let url = value.web {
+        return "\(physicalAddress) [\(url)]"
+      }
+      return physicalAddress
+    }
+  }
+}
+
+extension Location {
+  public func formatted() -> String {
+    Self.FormatStyle().format(self)
+  }
+
+  public func formatted<F: Foundation.FormatStyle>(_ style: F) -> F.FormatOutput
+  where F.FormatInput == Location {
+    style.format(self)
+  }
+}
+
+extension FormatStyle where Self == Location.FormatStyle {
+  public static var oneLine: Self { .init(.oneLine) }
+
+  static func Location(style: Location.FormatStyle.Style = .oneLine) -> Self {
+    .init(style)
+  }
+}
