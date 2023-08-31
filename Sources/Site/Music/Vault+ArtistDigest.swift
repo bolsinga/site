a1f52db1bd7596ceec4daa88e7b9950f81bbd057
Greg Bolsinga <bolsinga@mac.com>
2023-08-30 20:39:00 -0700

    format

--- Sources/Site/Music/Vault+ArtistDigest.swift
+++ Sources/Site/Music/Vault+ArtistDigest.swift
@@ -11,7 +11,8 @@ extension Vault {
   func digest(for artist: Artist) -> ArtistDigest {
     return ArtistDigest(
       artist: artist,
-      concerts: concerts.filter { $0.show.artists.contains(artist.id) }.sorted(by: comparator.compare(lhs:rhs:)),
+      concerts: concerts.filter { $0.show.artists.contains(artist.id) }.sorted(
+        by: comparator.compare(lhs:rhs:)),
       related: related(artist).sorted(by: comparator.libraryCompare(lhs:rhs:)))
   }
 }
