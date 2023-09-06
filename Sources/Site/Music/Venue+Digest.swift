293d3a480d2ee1407d5204a762ebe35ff98b52a4
Greg Bolsinga <bolsinga@mac.com>
2023-09-06 10:45:18 -0700

    Create Digests with comparator closure
    This way they are unaware of LibraryComparator, despite using it.

--- Sources/Site/Music/Venue+Digest.swift
+++ Sources/Site/Music/Venue+Digest.swift
@@ -9,14 +9,13 @@ import Foundation
 
 extension Array where Element == Venue {
   func digests(
-    concerts: [Concert], baseURL: URL?, lookup: Lookup, comparator: LibraryComparator
+    concerts: [Concert], baseURL: URL?, lookup: Lookup, comparator: (Concert, Concert) -> Bool
   ) -> [VenueDigest] {
     self.map { venue in
       VenueDigest(
         venue: venue,
         url: venue.archivePath.url(using: baseURL),
-        concerts: concerts.filter { $0.show.venue == venue.id }.sorted(
-          by: comparator.compare(lhs:rhs:)),
+        concerts: concerts.filter { $0.show.venue == venue.id }.sorted(by: comparator),
         related: lookup.related(venue),
         firstSet: lookup.firstSet(venue: venue),
         spanRank: lookup.spanRank(venue: venue),
