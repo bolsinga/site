d80b7cb885eaa3129eb4f1122e142016d50fb37a
Greg Bolsinga <bolsinga@mac.com>
2023-09-22 10:56:42 -0700

    Add AtlasCache
    - It reads from disk while initialized.
    - It writes to disk with each value added.
    - @NSCodingCodable wraps CLPlacemark so it is Codable to JSON
    - Values expire, and are "staggered" in their expiration so they do not all happen on the same day after expiring.
    - Fixes #238

---
+++ Sources/Site/Music/AtlasCache.swift
@@ -0,0 +1,69 @@
+//
+//  AtlasCache.swift
+//
+//
+//  Created by Greg Bolsinga on 9/22/23.
+//
+
+import CoreLocation
+import Foundation
+import os
+
+extension Logger {
+  static let atlasCache = Logger(category: "atlasCache")
+}
+
+//private let expirationOffset = 60.0 * 60.0 * 24.0 * 30.0 * 6.0  // Six months
+private let expirationOffset = 60.0 * 60.0 * 1.0  // One hour
+
+struct AtlasCache<T: AtlasGeocodable> {
+  struct Value: Codable {
+    @NSCodingCodable
+    var placemark: CLPlacemark
+    let expirationDate: Date
+  }
+
+  let fileName: String
+  let stagger: (() -> TimeInterval)  // This allows batch geocodes to be "staggered" in their expiration so they do not run all the time once the day comes.
+
+  var cache: [T: Value] = [:]
+
+  internal init(fileName: String = "atlas.json", stagger: @escaping (() -> TimeInterval)) {
+    self.fileName = fileName
+    self.stagger = stagger
+
+    do {
+      let diskCache = try [T: Value].read(fileName: fileName)
+      let now = Date.now
+      self.cache = diskCache.filter { $0.value.expirationDate >= now }  // Include those whose expiration date has not passed .now
+
+      if self.cache.count != diskCache.count {
+        Logger.atlasCache.error("removing expired items")
+        try self.cache.save(fileName: fileName)  // Some expired, so re-write the file.
+      }
+    } catch {
+      Logger.atlasCache.error("Cache Read Error: \(error, privacy: .public)")
+      self.cache = [:]
+    }
+  }
+
+  subscript(index: T) -> CLPlacemark? {
+    get {
+      cache[index]?.placemark
+    }
+    set(newValue) {
+      if let newValue {
+        cache[index] = Value(
+          placemark: newValue,
+          expirationDate: .now + expirationOffset + stagger())
+      } else {
+        cache[index] = nil
+      }
+      do {
+        try cache.save(fileName: fileName)
+      } catch {
+        Logger.atlasCache.error("Cache Save Error: \(error, privacy: .public)")
+      }
+    }
+  }
+}
---
+++ Sources/Site/Utility/NSCodingCodable.swift
@@ -0,0 +1,50 @@
+//
+//  NSCodingCodable.swift
+//
+//
+//  Created by Greg Bolsinga on 9/23/23.
+//
+
+import Foundation
+
+@propertyWrapper
+struct NSCodingCodable<T: NSObject & NSCoding>: Codable {
+  enum NSCodingCodableError: Error {
+    case decodeFailure
+  }
+
+  let wrappedValue: T
+
+  init(wrappedValue: T) {
+    self.wrappedValue = wrappedValue
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    let data = try container.decode(Data.self)
+
+    let unarchiver = try NSKeyedUnarchiver(forReadingFrom: data)
+    unarchiver.requiresSecureCoding = Self.supportsSecureCoding
+
+    guard let wrappedValue = T(coder: unarchiver) else {
+      throw NSCodingCodableError.decodeFailure
+    }
+
+    unarchiver.finishDecoding()
+
+    self.init(wrappedValue: wrappedValue)
+  }
+
+  func encode(to encoder: Encoder) throws {
+    let archiver = NSKeyedArchiver(requiringSecureCoding: Self.supportsSecureCoding)
+    wrappedValue.encode(with: archiver)
+    archiver.finishEncoding()
+
+    var container = encoder.singleValueContainer()
+    try container.encode(archiver.encodedData)
+  }
+
+  private static var supportsSecureCoding: Bool {
+    (T.self as? NSSecureCoding.Type)?.supportsSecureCoding ?? false
+  }
+}
