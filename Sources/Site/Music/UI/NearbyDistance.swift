f4fee73d9e8f98867778dc439b4889cbe66e81f1
Greg Bolsinga <bolsinga@mac.com>
2023-10-03 10:13:45 -0700

    Allow nearby distance threshold to be changed in the UI.

---
+++ Sources/Site/Music/UI/NearbyDistance.swift
@@ -0,0 +1,51 @@
+//
+//  NearbyDistance.swift
+//
+//
+//  Created by Greg Bolsinga on 10/8/23.
+//
+
+import CoreLocation
+
+private func createDistanceValueFormatter() -> MeasurementFormatter {
+  let formatter = MeasurementFormatter()
+  formatter.unitOptions = .naturalScale
+
+  let numberFormatter = NumberFormatter()
+  numberFormatter.numberStyle = .none
+  formatter.numberFormatter = numberFormatter
+
+  return formatter
+}
+
+private func createDistanceThresholdFormatter() -> MeasurementFormatter {
+  let formatter = MeasurementFormatter()
+  formatter.unitOptions = .naturalScale
+  formatter.unitStyle = .medium
+  return formatter
+}
+
+let nearbyDistanceValueFormatter = createDistanceValueFormatter()
+let nearbyDistanceThresholdFormatter = createDistanceThresholdFormatter()
+
+private let minimumDistanceInMiles = 1609.34  // 1 mile
+private let maximumDistanceInMiles = 160934.4  // 100 miles
+
+private let minimumDistanceInMetric = 1000.0  // 1 km
+private let maximumDistanceInMetric = 150000.0  // 150 km
+
+private var distanceUIIsMetric: Bool {
+  Locale.autoupdatingCurrent.measurementSystem == Locale.MeasurementSystem.metric
+}
+
+var minimumNearbyDistance: CLLocationDistance {
+  distanceUIIsMetric ? minimumDistanceInMetric : minimumDistanceInMiles
+}
+
+var maximumNearbyDistance: CLLocationDistance {
+  distanceUIIsMetric ? maximumDistanceInMetric : maximumDistanceInMiles
+}
+
+var nearbyDistanceStep: CLLocationDistance {
+  minimumNearbyDistance
+}
---
+++ Sources/Site/Music/UI/NearbyDistanceThresholdModifier.swift
@@ -0,0 +1,44 @@
+//
+//  NearbyDistanceThresholdModifier.swift
+//
+//
+//  Created by Greg Bolsinga on 10/3/23.
+//
+
+import CoreLocation
+import SwiftUI
+
+struct NearbyDistanceThresholdModifier: ViewModifier {
+  @Binding var distanceThreshold: CLLocationDistance
+  @State private var presentDistanceSliderPopover = false
+
+  func body(content: Content) -> some View {
+    content
+      .toolbar {
+        ToolbarItem(placement: .primaryAction) {
+          let labelText = Text(
+            "Nearby Distance", bundle: .module,
+            comment: "Label for changing the nearby concerts distance threshold.")
+          Button {
+            presentDistanceSliderPopover = true
+          } label: {
+            Label {
+              labelText
+            } icon: {
+              Image(systemName: "gear")
+            }
+          }
+          .popover(isPresented: $presentDistanceSliderPopover) {
+            NearbyDistanceThresholdView(distanceThreshold: $distanceThreshold) { labelText }
+              .presentationCompactAdaptation(.popover)
+          }
+        }
+      }
+  }
+}
+
+extension View {
+  func nearbyDistanceThreshold(_ distanceThreshold: Binding<CLLocationDistance>) -> some View {
+    modifier(NearbyDistanceThresholdModifier(distanceThreshold: distanceThreshold))
+  }
+}
