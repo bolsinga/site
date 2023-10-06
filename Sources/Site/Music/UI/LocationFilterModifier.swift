f5baaedab47266b6ef56a4e9961937f8c3ebe6a7
Greg Bolsinga <bolsinga@mac.com>
2023-10-06 09:19:47 -0700

    Display Location Authorization information in the LocationFilter

--- Sources/Site/Music/UI/LocationFilterModifier.swift
+++ Sources/Site/Music/UI/LocationFilterModifier.swift
@@ -10,16 +10,34 @@ import SwiftUI
 struct LocationFilterModifier: ViewModifier {
   @Binding var locationFilter: LocationFilter
   @Binding var geocodingProgress: Double
+  @Binding var locationAuthorization: LocationAuthorization
 
-  func body(content: Content) -> some View {
-    VStack {
-      if locationFilter.isNearby, geocodingProgress < 1.0 {
+  @ViewBuilder private var nearbyEnabledView: some View {
+    switch locationAuthorization {
+    case .allowed:
+      if geocodingProgress < 1.0 {
         ProgressView(value: geocodingProgress)
           .progressViewStyle(.circular)
           .tint(.accentColor)
           #if os(macOS)
             .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
           #endif
+      }
+    case .restricted:
+      Text(
+        "Location Disabled", bundle: .module,
+        comment: "Text shown when location services are restrictued by user.")
+    case .denied:
+      Text(
+        "Location Unavailable", bundle: .module,
+        comment: "Text shown when location services are denied.")
+    }
+  }
+
+  func body(content: Content) -> some View {
+    VStack {
+      if locationFilter.isNearby {
+        nearbyEnabledView
       }
       content
     }
@@ -35,10 +53,15 @@ struct LocationFilterModifier: ViewModifier {
 }
 
 extension View {
-  func locationFilter(_ locationFilter: Binding<LocationFilter>, geocodingProgress: Binding<Double>)
+  func locationFilter(
+    _ locationFilter: Binding<LocationFilter>, geocodingProgress: Binding<Double>,
+    locationAuthorization: Binding<LocationAuthorization>
+  )
     -> some View
   {
     modifier(
-      LocationFilterModifier(locationFilter: locationFilter, geocodingProgress: geocodingProgress))
+      LocationFilterModifier(
+        locationFilter: locationFilter, geocodingProgress: geocodingProgress,
+        locationAuthorization: locationAuthorization))
   }
 }
