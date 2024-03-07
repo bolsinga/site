11907c4d25e491362658ea3857b1322e3b5149de
Greg Bolsinga <bolsinga@mac.com>
2024-03-07 13:08:31 -0800

    decode activity logger for path is now a local variable

--- Sources/Site/Music/NSUserActivity+ArchivePath.swift
+++ Sources/Site/Music/NSUserActivity+ArchivePath.swift
@@ -10,7 +10,6 @@ import os
 
 extension Logger {
   public static let updateActivity = Logger(category: "updateActivity")
-  public static let decodeActivity = Logger(category: "decodeActivity")
 }
 
 extension NSUserActivity {
@@ -41,25 +40,25 @@ extension NSUserActivity {
     self.expirationDate = .now + (60 * 60 * 24)
   }
 
-  func archivePath() throws -> ArchivePath {
-    Logger.decodeActivity.log("type: \(self.activityType, privacy: .public)")
+  func archivePath(_ logger: Logger) throws -> ArchivePath {
+    logger.log("type: \(self.activityType, privacy: .public)")
 
     guard let userInfo = self.userInfo else {
-      Logger.decodeActivity.error("no userInfo")
+      logger.error("no userInfo")
       throw DecodeError.noUserInfo
     }
 
     guard let value = userInfo[NSUserActivity.archivePathKey] else {
-      Logger.decodeActivity.error("no archivePathKey")
+      logger.error("no archivePathKey")
       throw DecodeError.noArchiveKey
     }
 
     guard let archiveString = value as? String else {
-      Logger.decodeActivity.error("archivePathKey not String")
+      logger.error("archivePathKey not String")
       throw DecodeError.archiveKeyIncorrectType
     }
 
-    Logger.decodeActivity.log("decode: \(archiveString, privacy: .public)")
+    logger.log("decode: \(archiveString, privacy: .public)")
 
     return try ArchivePath(archiveString)
   }
