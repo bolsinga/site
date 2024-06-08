969776bbf4a3b6917e1b8256c60a8be096eab8f3
Greg Bolsinga <bolsinga@mac.com>
2024-06-08 15:17:02 -0700

    Move searching into ArtistList / VenueList (#805)
    - #794 broke when there were "no items" in that it was confused if there
    were none by location filter or if by search not finding anything

--- Sources/Site/Music/UI/ArtistList.swift
+++ Sources/Site/Music/UI/ArtistList.swift
@@ -11,13 +11,20 @@ struct ArtistList: View {
   let artistDigests: [ArtistDigest]
   let sectioner: LibrarySectioner
   @Binding var sort: RankingSort
+  @Binding var searchString: String
 
   var body: some View {
+    let digests = artistDigests.names(filteredBy: searchString)
     RankableSortList(
-      items: artistDigests, sectioner: sectioner,
+      items: digests, sectioner: sectioner,
       title: String(localized: "Artists", bundle: .module),
       associatedRankName: String(localized: "Sort By Venue Count", bundle: .module),
-      associatedRankSectionHeader: { $0.venuesCountView }, sort: $sort)
+      associatedRankSectionHeader: { $0.venuesCountView }, sort: $sort
+    )
+    .archiveSearchable(
+      searchPrompt: String(localized: "Artist Names", bundle: .module),
+      searchString: $searchString, contentsEmpty: digests.isEmpty
+    )
   }
 }
 
@@ -25,7 +32,7 @@ struct ArtistList: View {
   NavigationStack {
     ArtistList(
       artistDigests: vaultPreviewData.artistDigests, sectioner: vaultPreviewData.sectioner,
-      sort: .constant(.alphabetical)
+      sort: .constant(.alphabetical), searchString: .constant("")
     )
     .musicDestinations(vaultPreviewData)
   }
