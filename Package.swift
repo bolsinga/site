308bb201e713cf937693d8763480f92c7f0035e8
Greg Bolsinga <bolsinga@mac.com>
2024-06-25 09:45:41 -0700

    Revert "backup to .4 releases since github does not have .5 (#820)" (#822)
    This reverts commit 1ef924934f610c420773c76118b9b8ed4457a8bc.
    
    This will work now that github actions are using Xcode 15.4

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
   name: "site",
   defaultLocalization: "en",
   platforms: [
-    .macOS("14.4"),
-    .iOS("17.4"),
-    .tvOS("17.4"),
+    .macOS("14.5"),
+    .iOS("17.5"),
+    .tvOS("17.5"),
   ],
   products: [
     .library(name: "Site", targets: ["Site"]),
