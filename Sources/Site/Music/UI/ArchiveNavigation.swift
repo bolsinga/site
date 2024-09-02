7f82b7014449226909fa90896bd6a3b21f29e00f
Greg Bolsinga <bolsinga@mac.com>
2024-09-02 08:51:48 -0700

    Use .defaultCategory where appropriate

--- Sources/Site/Music/UI/ArchiveNavigation.swift
+++ Sources/Site/Music/UI/ArchiveNavigation.swift
@@ -21,7 +21,7 @@ extension Logger {
 
   @ObservationIgnored internal var pendingNavigationPath: [ArchivePath]?
 
-  internal init(selectedCategory: ArchiveCategory? = nil, navigationPath: [ArchivePath] = []) {
+  internal init(selectedCategory: ArchiveCategory? = .defaultCategory, navigationPath: [ArchivePath] = []) {
     self.selectedCategory = selectedCategory
     self.navigationPath = navigationPath
     self.pendingNavigationPath = nil
