31f5cd49d65470b656aa36f5fb7e6b5b4666d180
Greg Bolsinga <bolsinga@mac.com>
2024-10-01 14:11:40 -0700

    Allow optional items to just handle things simply. (#930)

--- Sources/Site/Music/UI/ArchiveSharableModifier.swift
+++ Sources/Site/Music/UI/ArchiveSharableModifier.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ArchiveSharableModifier<T: ArchiveSharable>: ViewModifier {
-  let item: T
+  let item: T?
   let url: URL?
 
   func body(content: Content) -> some View {
     #if os(tvOS)
       content  // ShareLink not available on tvOS
     #else
-      if let url {
+      if let url, let item {
         content
           .toolbar {
             ToolbarItem(placement: .primaryAction) {
@@ -32,11 +32,11 @@ struct ArchiveSharableModifier<T: ArchiveSharable>: ViewModifier {
 }
 
 extension View {
-  func archiveShare<T: ArchiveSharable & Linkable>(_ item: T) -> some View {
-    modifier(ArchiveSharableModifier(item: item, url: item.url))
+  func archiveShare<T: ArchiveSharable & Linkable>(_ item: T?) -> some View {
+    modifier(ArchiveSharableModifier(item: item, url: item?.url))
   }
 
-  func archiveShare<T: ArchiveSharable>(_ item: T, url: URL?) -> some View {
+  func archiveShare<T: ArchiveSharable>(_ item: T?, url: URL?) -> some View {
     modifier(ArchiveSharableModifier(item: item, url: url))
   }
 }
