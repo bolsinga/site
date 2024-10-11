b7c0867298cf8230594f7cc6203f1acd940e22bf
Greg Bolsinga <bolsinga@mac.com>
2024-10-11 12:02:31 -0700

    Add extension on ArchivePath to get destination View. (#958)

--- Sources/Site/Music/UI/MusicDestinationModifier.swift
+++ Sources/Site/Music/UI/MusicDestinationModifier.swift
@@ -14,30 +14,8 @@ struct MusicDestinationModifier: ViewModifier {
 
   func body(content: Content) -> some View {
     content
-      .navigationDestination(for: ArchivePath.self) { archivePath in
-        switch archivePath {
-        case .show(let iD):
-          if let concert = vault.concertMap[iD] {
-            ShowDetail(concert: concert, isPathNavigable: isPathNavigable)
-          }
-        case .venue(let iD):
-          if let venueDigest = vault.venueDigestMap[iD] {
-            VenueDetail(
-              digest: venueDigest, concertCompare: vault.comparator.compare(lhs:rhs:),
-              geocode: { try await vault.atlas.geocode($0.venue) }, isPathNavigable: isPathNavigable
-            )
-          }
-        case .artist(let iD):
-          if let artistDigest = vault.artistDigestMap[iD] {
-            ArtistDetail(
-              digest: artistDigest, concertCompare: vault.comparator.compare(lhs:rhs:),
-              isPathNavigable: isPathNavigable)
-          }
-        case .year(let annum):
-          YearDetail(
-            digest: vault.digest(for: annum), concertCompare: vault.comparator.compare(lhs:rhs:),
-            isPathNavigable: isPathNavigable)
-        }
+      .navigationDestination(for: ArchivePath.self) {
+        $0.destination(vault: vault, isPathNavigable: isPathNavigable)
       }
   }
 }
