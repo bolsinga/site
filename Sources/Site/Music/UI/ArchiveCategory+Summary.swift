917c683b431de210d875e8b55e02db3990435657
Greg Bolsinga <bolsinga@mac.com>
2024-10-11 15:10:07 -0700

    Add ArchiveCategory to Summary view function

---
+++ Sources/Site/Music/UI/ArchiveCategory+Summary.swift
@@ -0,0 +1,37 @@
+//
+//  ArchiveCategory+Summary.swift
+//  site
+//
+//  Created by Greg Bolsinga on 10/11/24.
+//
+
+import SwiftUI
+
+extension ArchiveCategory {
+  typealias SortSearch = (sort: Binding<RankingSort>, search: Binding<String>)
+
+  @MainActor
+  @ViewBuilder func summary(
+    model: VaultModel, nearbyModel: NearbyModel, venueSortSearch: () -> SortSearch,
+    artistSortSearch: () -> SortSearch
+  ) -> some View {
+    switch self {
+    case .today:
+      TodaySummary(model: model)
+    case .stats:
+      StatsSummary(vault: model.vault)
+    case .shows:
+      ShowsSummary(model: model, nearbyModel: nearbyModel)
+    case .venues:
+      let sortSearch = venueSortSearch()
+      VenuesSummary(
+        model: model, nearbyModel: nearbyModel, sort: sortSearch.sort,
+        searchString: sortSearch.search)
+    case .artists:
+      let sortSearch = artistSortSearch()
+      ArtistsSummary(
+        model: model, nearbyModel: nearbyModel, sort: sortSearch.sort,
+        searchString: sortSearch.search)
+    }
+  }
+}
