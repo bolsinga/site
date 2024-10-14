9a72faac14466ab5df0d49941b38430fd50209c4
Greg Bolsinga <bolsinga@mac.com>
2024-10-14 12:54:08 -0700

    Add ArchiveCategoryRoot, which is the root of the NavigationStack.

---
+++ Sources/Site/Music/UI/ArchiveCategoryRoot.swift
@@ -0,0 +1,92 @@
+//
+//  ArchiveCategoryRoot.swift
+//  site
+//
+//  Created by Greg Bolsinga on 10/14/24.
+//
+
+import SwiftUI
+
+struct ArchiveCategoryRoot: View {
+  let model: VaultModel
+  let category: ArchiveCategory
+  @Binding var venueSort: RankingSort
+  @Binding var artistSort: RankingSort
+  let nearbyModel: NearbyModel
+
+  @State var artistSearchString: String = ""
+  @State var venueSearchString: String = ""
+
+  private func sortableData(_ category: ArchiveCategory) -> (
+    sort: Binding<RankingSort>, associatedRankName: String
+  )? {
+    switch category {
+    case .today, .stats, .shows:
+      nil
+    case .venues:
+      ($venueSort, String(localized: "Sort By Artist Count", bundle: .module))
+    case .artists:
+      ($artistSort, String(localized: "Sort By Venue Count", bundle: .module))
+    }
+  }
+
+  var body: some View {
+    category.summary(
+      model: model, nearbyModel: nearbyModel,
+      venueSortSearch: {
+        (venueSort, $venueSearchString)
+      },
+      artistSortSearch: {
+        (artistSort, $artistSearchString)
+      }
+    )
+    .toolbar {
+      if category.isLocationFilterable {
+        @Bindable var bindableNearbyModel = nearbyModel
+        LocationFilterToolbarContent(isOn: $bindableNearbyModel.locationFilter.toggle)
+      }
+      if let sortableData = sortableData(category) {
+        SortModifierToolbarContent(algorithm: sortableData.sort) {
+          switch $0 {
+          case .associatedRank:
+            return sortableData.associatedRankName
+          default:
+            return $0.localizedString
+          }
+        }
+      }
+      ArchiveSharableToolbarContent(
+        item: ArchiveCategoryLinkable(vault: model.vault, category: category))
+    }
+  }
+}
+
+// Preview only extension
+extension ArchiveCategoryRoot {
+  init(withPreviewCategory category: ArchiveCategory) {
+    self.init(
+      model: VaultModel(vaultPreviewData, executeAsynchronousTasks: false),
+      category: category, venueSort: .constant(.alphabetical), artistSort: .constant(.alphabetical),
+      nearbyModel: NearbyModel())
+  }
+}
+
+#Preview {
+  ArchiveCategoryRoot(withPreviewCategory: .today)
+}
+
+#Preview {
+  ArchiveCategoryRoot(withPreviewCategory: .stats)
+}
+
+#Preview {
+  ArchiveCategoryRoot(withPreviewCategory: .shows)
+}
+
+#Preview {
+  ArchiveCategoryRoot(withPreviewCategory: .venues)
+}
+
+#Preview {
+  ArchiveCategoryRoot(withPreviewCategory: .artists)
+}
