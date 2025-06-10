f8e78d206c658332cfbc2e62e07e7e9e6be99f81
Greg Bolsinga <bolsinga@mac.com>
2025-06-10 15:07:18 -0700

    separate geocoding throttling error into its own file

---
+++ Sources/Site/Music/NSError+Throttle.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  site
+//
+//  Created by Greg Bolsinga on 6/10/25.
+//
+
+import CoreLocation
+
+extension NSError {
+  var isGeocodingThrottledError: Bool {
+    self.code == CLError.network.rawValue && self.domain == kCLErrorDomain
+  }
+}
