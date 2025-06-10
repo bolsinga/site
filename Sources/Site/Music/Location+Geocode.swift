29cfc63a84bf5a6dcf257ea8411e9adfbfcb0cbf
Greg Bolsinga <bolsinga@mac.com>
2025-06-10 14:49:28 -0700

    Locations are no longer AtlasGeocodable
    They do not need to be. In the newer geocoding code in OS 26, more informaton from Venue may be passed, so Location will have some, but not all of the data that would be useful for Geocoding.

--- Sources/Site/Music/Location+Geocode.swift
+++ Sources/Site/Music/Location+Geocode.swift
@@ -12,7 +12,7 @@ import Foundation
   import Contacts
 #endif
 
-extension Location: AtlasGeocodable {
+extension Location {
   #if canImport(Contacts)
     private var postalAddress: CNPostalAddress {
       let pAddress = CNMutablePostalAddress()
