c254979e806c266a7ca65ef9c34b0c532e0107ba
Greg Bolsinga <bolsinga@mac.com>
2025-07-23 14:37:11 -0700

    Use Geocodable more often (#1026)

--- Sources/Site/Music/Geocode.swift
+++ Sources/Site/Music/Geocode.swift
@@ -11,20 +11,24 @@ private enum GeocodeError: Error {
   case noPlacemark
 }
 
-func geocodeAddressString(_ addressString: String) async throws -> Placemark {
-  guard let placemark = try await CLGeocoder().geocodeAddressString(addressString).first else {
-    throw GeocodeError.noPlacemark
+extension String: Geocodable {
+  func geocode() async throws -> Placemark {
+    guard let placemark = try await CLGeocoder().geocodeAddressString(self).first else {
+      throw GeocodeError.noPlacemark
+    }
+    return Placemark(placemark: placemark)
   }
-  return Placemark(placemark: placemark)
 }
 
 #if canImport(Contacts)
   import Contacts
 
-  func geocodePostalAddress(_ address: CNPostalAddress) async throws -> Placemark {
-    guard let placemark = try await CLGeocoder().geocodePostalAddress(address).first else {
-      throw GeocodeError.noPlacemark
+  extension CNPostalAddress: Geocodable {
+    func geocode() async throws -> Placemark {
+      guard let placemark = try await CLGeocoder().geocodePostalAddress(self).first else {
+        throw GeocodeError.noPlacemark
+      }
+      return Placemark(placemark: placemark)
     }
-    return Placemark(placemark: placemark)
   }
 #endif
