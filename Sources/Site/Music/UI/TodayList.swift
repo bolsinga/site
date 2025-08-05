e7d16049da670cd8b469767454018ee463fc475d
Greg Bolsinga <bolsinga@mac.com>
2025-08-05 16:41:52 -0700

    Use a Section header for Today title

--- Sources/Site/Music/UI/TodayList.swift
+++ Sources/Site/Music/UI/TodayList.swift
@@ -18,14 +18,18 @@ struct TodayList: View {
         description: Text("Check again tomorrow.", bundle: .module)
       )
     } else {
-      List(concerts) { concert in
-        NavigationLink(value: concert) { TodayBlurb(concert: concert) }
-      }
-      .listStyle(.plain)
-      .navigationTitle(
+      Section {
+        List(concerts) { concert in
+          NavigationLink(value: concert) { TodayBlurb(concert: concert) }
+        }
+        .listStyle(.plain)
+      } header: {
         Text(
           "On This Day: \(Date.now.formatted(.dateTime.month(.defaultDigits).day()))",
-          bundle: .module))
+          bundle: .module
+        )
+        .font(.title)
+      }
     }
   }
 }
