c00accaf17d6ca2ef8ee48258ff2d5099f849eb0
Greg Bolsinga <bolsinga@mac.com>
2025-09-02 13:28:08 -0700

    Make Ranking public

--- Sources/Site/Utility/Ranking.swift
+++ Sources/Site/Utility/Ranking.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-struct Ranking: Comparable, Hashable, Sendable {
-  let rank: Rank
-  let value: Int
-
-  static var empty: Ranking {
+public struct Ranking: Comparable, Hashable, Sendable {
+  public let rank: Rank
+  public let value: Int
+  
+  public static var empty: Ranking {
     Ranking(rank: .unknown, value: 0)
   }
 
-  static func < (lhs: Ranking, rhs: Ranking) -> Bool {
+  public static func < (lhs: Ranking, rhs: Ranking) -> Bool {
     if lhs.rank == rhs.rank {
       return lhs.value < rhs.value
     }
