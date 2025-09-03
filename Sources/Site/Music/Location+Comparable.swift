58043798e224818e1add289ffccaba4a57b25731
Greg Bolsinga <bolsinga@mac.com>
2025-09-03 14:06:23 -0700

    Declare Comparable conformance in a separate file

---
+++ Sources/Site/Music/Location+Comparable.swift
@@ -0,0 +1,18 @@
+//
+//  Location+Comparable.swift
+//  site
+//
+//  Created by Greg Bolsinga on 9/3/25.
+//
+
+extension Location: Comparable {
+  public static func < (lhs: Location, rhs: Location) -> Bool {
+    if lhs.state == rhs.state {
+      if lhs.city == rhs.city {
+        return compareOptional(lhs: lhs.street, rhs: rhs.street)
+      }
+      return lhs.city < rhs.city
+    }
+    return lhs.state < rhs.state
+  }
+}
