c963f45e975a42433a6dde9e75dbbff9e2e7a30c
Greg Bolsinga <bolsinga@mac.com>
2025-09-07 15:27:36 -0700

    Add SiteApp

---
+++ Sources/App/SiteAppApp.swift
@@ -0,0 +1,28 @@
+//
+//  SiteAppApp.swift
+//  SiteApp
+//
+//  Created by Greg Bolsinga on 3/24/23.
+//
+
+import Site
+import SwiftUI
+
+@main
+struct SiteAppApp: App {
+  @State private var model = SiteModel(urlString: "https://www.bolsinga.com/json/shows.json")
+
+  var body: some Scene {
+    WindowGroup {
+      SiteView(model)
+    }
+    #if !os(tvOS)
+      .commands {
+        RefreshCommand { await model.load() }
+      }
+    #endif
+    #if os(macOS)
+      Settings { SettingsView() }
+    #endif
+  }
+}
