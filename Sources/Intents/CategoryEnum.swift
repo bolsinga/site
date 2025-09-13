ee9eabf73e69204b3c6c4459eac35df0f2a3b15a
Greg Bolsinga <bolsinga@mac.com>
2025-09-12 18:23:46 -0700

    Add (Type)DisplayRepresentation synonyms (#1150)

--- Sources/Intents/CategoryEnum.swift
+++ Sources/Intents/CategoryEnum.swift
@@ -23,9 +23,12 @@ extension CategoryEnum: AppEnum {
   public static let caseDisplayRepresentations: [CategoryEnum: DisplayRepresentation] = [
     .today: DisplayRepresentation(title: "Today", image: .init(systemName: "calendar.circle")),
     .shows: DisplayRepresentation(
-      title: "Shows", image: .init(systemName: "person.and.background.dotted")),
-    .venues: DisplayRepresentation(title: "Venues", image: .init(systemName: "music.note.house")),
-    .artists: DisplayRepresentation(title: "Artists", image: .init(systemName: "music.mic")),
+      title: "Shows", image: .init(systemName: "person.and.background.dotted"),
+      synonyms: ["Concerts"]),
+    .venues: DisplayRepresentation(
+      title: "Venues", image: .init(systemName: "music.note.house"), synonyms: ["Clubs"]),
+    .artists: DisplayRepresentation(
+      title: "Artists", image: .init(systemName: "music.mic"), synonyms: ["Performers", "Bands"]),
   ]
 }
 
