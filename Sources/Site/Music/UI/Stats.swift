480f83d1905d125635eba72db6c9e0f85ddc521e
Greg Bolsinga <bolsinga@mac.com>
2025-09-17 15:54:45 -0700

    stats separation

---
+++ Sources/Site/Music/UI/Stats.swift
@@ -0,0 +1,92 @@
+//
+//  Stats.swift
+//  SiteApp
+//
+//  Created by Greg Bolsinga on 9/17/25.
+//
+
+import Foundation
+
+extension Collection where Element == Concert {
+  fileprivate var stateCounts: [String: Int] {
+    self.compactMap { $0.venue?.location }.map { $0.state }.reduce(
+      into: [String: Int]()
+    ) {
+      let count = $0[$1] ?? 0
+      $0[$1] = count + 1
+    }
+  }
+
+  fileprivate var knownDates: [Date] {
+    self.map { $0.show.date }
+      .filter { $0.day != nil }
+      .filter { $0.month != nil }
+      .filter { $0.year != nil }
+      .compactMap { $0.date }
+  }
+
+  fileprivate var venues: [Venue] {
+    Array(self.compactMap { $0.venue }.uniqued())
+  }
+
+  fileprivate var artists: [Artist] {
+    Array(self.flatMap { $0.artists }.uniqued())
+  }
+}
+
+struct Stats {
+  let concerts: [Concert]
+  let venues: [Venue]
+  let artists: [Artist]
+  let dates: [Date]
+  let stateCounts: [String: Int]
+
+  internal init(concerts: [Concert], shouldCalculateArtistCount: Bool) {
+    self.concerts = concerts
+    self.dates = concerts.knownDates
+    self.venues = concerts.venues
+    self.artists = shouldCalculateArtistCount ? concerts.artists : []
+    self.stateCounts = concerts.stateCounts
+  }
+
+  func categories(
+    weekdayOrMonthChartConcertThreshold: Int, displayArchiveCategoryCounts: Bool,
+    yearsSpanRanking: Ranking?
+  ) -> [StatsCategory] {
+    let venuesCount = venues.count
+    let showVenues = venuesCount > 1
+
+    let artistsCount = artists.count
+    let showArtists = artistsCount > 1
+
+    let showState = stateCounts.keys.count > 1
+
+    let showWeekdayOrMonthChart = concerts.count > weekdayOrMonthChartConcertThreshold
+
+    // This needs to be broken down like this or the swift compiler says it is too complex.
+    var statsCategoryCases = [StatsCategory]()
+    if displayArchiveCategoryCounts {
+      statsCategoryCases.append(.shows)
+    }
+    if displayArchiveCategoryCounts && (yearsSpanRanking?.value ?? 0) > 1 {
+      statsCategoryCases.append(.years)
+    }
+    if displayArchiveCategoryCounts && showVenues {
+      statsCategoryCases.append(.venues)
+    }
+    if displayArchiveCategoryCounts && showArtists {
+      statsCategoryCases.append(.artists)
+    }
+    if showWeekdayOrMonthChart {
+      statsCategoryCases.append(.weekday)
+    }
+    if showWeekdayOrMonthChart {
+      statsCategoryCases.append(.month)
+    }
+    if showState {
+      statsCategoryCases.append(.state)
+    }
+
+    return statsCategoryCases
+  }
+}
--- Sources/Site/Music/UI/StatsGrouping.swift
+++ Sources/Site/Music/UI/StatsGrouping.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct StatsGrouping: View {
   @Environment(\.statsThreshold) private var statsThreshold: Int
 
-  let concerts: [Concert]
-  let shouldCalculateArtistCount: Bool
+  let stats: Stats
+
   let yearsSpanRanking: Ranking?
   let showRanking: Ranking?
   let artistVenuesRanking: Ranking?
@@ -27,22 +27,12 @@ struct StatsGrouping: View {
     venueArtistsRanking: Ranking? = nil,
     displayArchiveCategoryCounts: Bool = true
   ) {
-    self.concerts = concerts
-    self.shouldCalculateArtistCount = shouldCalculateArtistCount
+    self.stats = Stats(concerts: concerts, shouldCalculateArtistCount: shouldCalculateArtistCount)
     self.yearsSpanRanking = yearsSpanRanking
     self.showRanking = showRanking
     self.artistVenuesRanking = artistVenuesRanking
     self.venueArtistsRanking = venueArtistsRanking
     self.displayArchiveCategoryCounts = displayArchiveCategoryCounts
-  }
-
-  private var computedStateCounts: [String: Int] {
-    concerts.compactMap { $0.venue?.location }.map { $0.state }.reduce(
-      into: [String: Int]()
-    ) {
-      let count = $0[$1] ?? 0
-      $0[$1] = count + 1
-    }
   }
 
   @ViewBuilder var yearsElement: some View {
@@ -55,16 +45,8 @@ struct StatsGrouping: View {
     }
   }
 
-  private var computeVenues: [Venue] {
-    Array(Set(concerts.compactMap { $0.venue }))
-  }
-
-  private var computeArtists: [Artist] {
-    Array(Set(concerts.flatMap { $0.artists }))
-  }
-
   @ViewBuilder var showCount: some View {
-    Text("\(concerts.count) Show(s)")
+    Text("\(stats.concerts.count) Show(s)")
   }
 
   @ViewBuilder var showsElement: some View {
@@ -111,55 +93,11 @@ struct StatsGrouping: View {
     }
   }
 
-  private func configure() -> ([StatsCategory], [Venue], [Artist], [Date], [String: Int]) {
-    let knownShowDates = concerts.map { $0.show.date }.filter { $0.day != nil }
-      .filter { $0.month != nil }
-      .filter { $0.year != nil }
-      .compactMap { $0.date }
-
-    let venues = computeVenues
-    let venuesCount = venues.count
-    let showVenues = venuesCount > 1
-
-    let artists = shouldCalculateArtistCount ? computeArtists : []
-    let artistsCount = artists.count
-    let showArtists = artistsCount > 1
-
-    let stateCounts = computedStateCounts
-    let showState = stateCounts.keys.count > 1
-
-    let showWeekdayOrMonthChart = concerts.count > statsThreshold
-
-    // This needs to be broken down like this or the swift compiler says it is too complex.
-    var statsCategoryCases = [StatsCategory]()
-    if displayArchiveCategoryCounts {
-      statsCategoryCases.append(.shows)
-    }
-    if displayArchiveCategoryCounts && (yearsSpanRanking?.value ?? 0) > 1 {
-      statsCategoryCases.append(.years)
-    }
-    if displayArchiveCategoryCounts && showVenues {
-      statsCategoryCases.append(.venues)
-    }
-    if displayArchiveCategoryCounts && showArtists {
-      statsCategoryCases.append(.artists)
-    }
-    if showWeekdayOrMonthChart {
-      statsCategoryCases.append(.weekday)
-    }
-    if showWeekdayOrMonthChart {
-      statsCategoryCases.append(.month)
-    }
-    if showState {
-      statsCategoryCases.append(.state)
-    }
-
-    return (statsCategoryCases, venues, artists, knownShowDates, stateCounts)
-  }
-
   var body: some View {
-    let (statsCategoryCases, venues, artists, knownShowDates, stateCounts) = configure()
-
+    let statsCategoryCases = stats.categories(
+      weekdayOrMonthChartConcertThreshold: statsThreshold,
+      displayArchiveCategoryCounts: displayArchiveCategoryCounts, yearsSpanRanking: yearsSpanRanking
+    )
     ForEach(statsCategoryCases, id: \.self) { category in
       Group {
         switch category {
@@ -168,21 +106,21 @@ struct StatsGrouping: View {
         case .years:
           yearsElement
         case .venues:
-          element(for: venues)
+          element(for: stats.venues)
         case .artists:
-          element(for: artists)
+          element(for: stats.artists)
         case .weekday:
           let name = String(localized: "Weekdays")
-          NavigationLink(name) { WeekdayChart(dates: knownShowDates).navigationTitle(name) }
+          NavigationLink(name) { WeekdayChart(dates: stats.dates).navigationTitle(name) }
         case .month:
           let name = String(localized: "Months")
-          NavigationLink(name) { MonthChart(dates: knownShowDates).navigationTitle(name) }
+          NavigationLink(name) { MonthChart(dates: stats.dates).navigationTitle(name) }
         case .state:
           NavigationLink {
-            StateChart(counts: stateCounts)
+            StateChart(counts: stats.stateCounts)
           } label: {
             LabeledContent {
-              Text(stateCounts.keys.count.formatted())
+              Text(stats.stateCounts.keys.count.formatted())
             } label: {
               Text("States")
             }
