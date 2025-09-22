896db77127fcc83236034004a9185837c47136fb
Greg Bolsinga <bolsinga@mac.com>
2025-09-22 15:51:33 -0700

    MonthChart uses Date and formats based upon user interface size.

--- Sources/Site/Music/UI/MonthChart.swift
+++ Sources/Site/Music/UI/MonthChart.swift
@@ -9,20 +9,33 @@ import Charts
 import SwiftUI
 
 struct MonthChart: View {
+  @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
   let dates: [Date]
+
+  private var format: Date.FormatStyle.Symbol.Month {
+    horizontalSizeClass == .compact ? .narrow : .abbreviated
+  }
 
   var body: some View {
     let monthCounts = dates.monthCounts.sorted { $0.key < $1.key }  // array of dictionary elements
     Chart(monthCounts, id: \.key) { item in
       BarMark(
-        x: .value(Text("Month"), item.value.0),
-        y: .value(Text("Count"), item.value.1)
+        x: .value("Month", item.value.0, unit: .month),
+        y: .value("Count", item.value.1)
       )
       .annotation(position: .top) {
         if item.value.1 > 0 {
           Text(item.value.1.formatted(.number))
             .font(.caption2)
         }
+      }
+    }
+    .chartXAxis {
+      AxisMarks(values: .stride(by: .month)) { _ in
+        AxisGridLine()
+        AxisTick()
+        AxisValueLabel(format: .dateTime.month(format), centered: true)
       }
     }
   }
