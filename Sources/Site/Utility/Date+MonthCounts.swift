cbf6e8e988d9cffc0b8f88f7cf89bff287612951
Greg Bolsinga <bolsinga@mac.com>
2025-09-23 12:34:15 -0700

    MonthChart uses Date and formats based upon user interface size. (#1184)

--- Sources/Site/Utility/Date+MonthCounts.swift
+++ Sources/Site/Utility/Date+MonthCounts.swift
@@ -7,27 +7,24 @@
 
 import Foundation
 
-private let MonthChartFormat = Date.FormatStyle.dateTime.month(.abbreviated)
-
-// month as int: month as formatted localized string
-private var MonthAbbreviationsGetter: [Int: (String, Int)] {
-  var result = [Int: (String, Int)]()
+// month as int: (month as Date, count for that month)
+private var monthAbbreviations: [Int: (Date, Int)] {
+  var result = [Int: (Date, Int)]()
   for month in 1...12 {
     guard
       let date = DateComponents(calendar: Calendar.current, year: 2023, month: month, day: 1).date
     else { fatalError("need to be able to get abbreviated months") }
-    result[month] = (MonthChartFormat.format(date), 0)
+    result[month] = (date, 0)
   }
   return result
 }
 
-private let MonthAbbreviations = MonthAbbreviationsGetter
-
 extension Array where Element == Date {
-  var monthCounts: [Int: (String, Int)] {  // month as int: (month as string, count for that month)
-    reduce(into: MonthAbbreviations) {
+  // month as int: (month as Date, count for that month)
+  var monthCounts: [Int: (Date, Int)] {
+    reduce(into: monthAbbreviations) {
       let month = Calendar.autoupdatingCurrent.component(.month, from: $1)
-      let pair = $0[month] ?? (MonthChartFormat.format($1), 0)
+      let pair = $0[month] ?? ($1, 0)
       $0[month] = (pair.0, pair.1 + 1)
     }
   }
