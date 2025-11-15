80d3967d567ec2083ab820316d9cd0d4d99539fd
Greg Bolsinga <bolsinga@mac.com>
2025-11-14 19:52:27 -0800

    Use MKAddress to initialize MKMapItem on OS 26 (#1231)

--- Sources/Site/Music/Venue+MapItem.swift
+++ Sources/Site/Music/Venue+MapItem.swift
@@ -11,14 +11,26 @@ import MapKit
   import Contacts
 #endif
 
+extension Venue {
+  @available(iOS 26.0, macOS 26.0, tvOS 26.0, *)
+  fileprivate var mkAddress: MKAddress? {
+    MKAddress(fullAddress: location.addressString, shortAddress: nil)
+  }
+}
+
 extension MKMapItem {
   convenience init(venue: Venue, location: CLLocation) {
-    #if canImport(Contacts)
-      let placemark = MKPlacemark(
-        location: location, name: venue.name, postalAddress: venue.location.postalAddress)
-    #else
-      let placemark = MKPlacemark(coordinate: location.coordinate)
-    #endif
-    self.init(placemark: placemark)
+    if #available(iOS 26, macOS 26, tvOS 26, *) {
+      self.init(location: location, address: venue.mkAddress)
+      self.name = venue.name
+    } else {
+      #if canImport(Contacts)
+        let placemark = MKPlacemark(
+          location: location, name: venue.name, postalAddress: venue.location.postalAddress)
+      #else
+        let placemark = MKPlacemark(coordinate: location.coordinate)
+      #endif
+      self.init(placemark: placemark)
+    }
   }
 }
