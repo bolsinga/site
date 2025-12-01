e6b7d251838d3c3598b5bec7c31d2cc3e543419d
Greg Bolsinga <bolsinga@mac.com>
2025-12-01 09:24:19 -0800

    Clarify what is wanted and what may be optional.

--- Sources/Site/Music/Atlas+MKMapItem.swift
+++ Sources/Site/Music/Atlas+MKMapItem.swift
@@ -10,7 +10,7 @@ import MapKit
 
 extension Atlas where T == Venue {
   nonisolated func geocode(_ venue: T) async throws -> MKMapItem? {
-    guard let location = try await geocode(venue)?.location else { return nil }
-    return MKMapItem(venue: venue, location: location)
+    guard let placemark: Placemark = try await geocode(venue) else { return nil }
+    return MKMapItem(venue: venue, location: placemark.location)
   }
 }
