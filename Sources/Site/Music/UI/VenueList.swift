bde58d15796924ba668247c4a19fb5239473c4f8
Greg Bolsinga <bolsinga@mac.com>
2025-12-13 18:57:23 -0800

    add more previews for RankableSortList variants

--- Sources/Site/Music/UI/VenueList.swift
+++ Sources/Site/Music/UI/VenueList.swift
@@ -28,10 +28,62 @@ struct VenueList: View {
   }
 }
 
-#Preview(traits: .vaultModel) {
+#Preview("alphabetical", traits: .vaultModel) {
   @Previewable @Environment(VaultModel.self) var model
-  VenueList(
-    venueDigests: model.vault.venueDigests, sectioner: model.vault.sectioner,
-    sort: .alphabetical, searchString: .constant("")
-  )
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    VenueList(
+      venueDigests: model.vault.venueDigests, sectioner: model.vault.sectioner,
+      sort: .alphabetical, searchString: $searchString
+    )
+  }
+}
+
+#Preview("showCount", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    VenueList(
+      venueDigests: model.vault.venueDigests, sectioner: model.vault.sectioner,
+      sort: .showCount, searchString: $searchString
+    )
+  }
+}
+
+#Preview("showYearRange", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    VenueList(
+      venueDigests: model.vault.venueDigests, sectioner: model.vault.sectioner,
+      sort: .showYearRange, searchString: $searchString
+    )
+  }
+}
+
+#Preview("associatedRank", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    VenueList(
+      venueDigests: model.vault.venueDigests, sectioner: model.vault.sectioner,
+      sort: .associatedRank, searchString: $searchString
+    )
+  }
+}
+
+#Preview("firstSeen", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    VenueList(
+      venueDigests: model.vault.venueDigests, sectioner: model.vault.sectioner,
+      sort: .firstSeen, searchString: $searchString
+    )
+  }
 }
