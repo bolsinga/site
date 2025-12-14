6e7ac4ac8396d7153543d69ed4ae9baec9fe0a46
Greg Bolsinga <bolsinga@mac.com>
2025-12-13 19:35:17 -0800

    add more previews for RankableSortList variants (#1245)

--- Sources/Site/Music/UI/ArtistList.swift
+++ Sources/Site/Music/UI/ArtistList.swift
@@ -28,10 +28,62 @@ struct ArtistList: View {
   }
 }
 
-#Preview(traits: .vaultModel) {
+#Preview("alphabetical", traits: .vaultModel) {
   @Previewable @Environment(VaultModel.self) var model
-  ArtistList(
-    artistDigests: model.vault.artistDigests, sectioner: model.vault.sectioner,
-    sort: .alphabetical, searchString: .constant("")
-  )
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    ArtistList(
+      artistDigests: model.vault.artistDigests, sectioner: model.vault.sectioner,
+      sort: .alphabetical, searchString: $searchString
+    )
+  }
+}
+
+#Preview("showCount", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    ArtistList(
+      artistDigests: model.vault.artistDigests, sectioner: model.vault.sectioner,
+      sort: .showCount, searchString: $searchString
+    )
+  }
+}
+
+#Preview("showYearRange", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    ArtistList(
+      artistDigests: model.vault.artistDigests, sectioner: model.vault.sectioner,
+      sort: .showYearRange, searchString: $searchString
+    )
+  }
+}
+
+#Preview("associatedRank", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    ArtistList(
+      artistDigests: model.vault.artistDigests, sectioner: model.vault.sectioner,
+      sort: .associatedRank, searchString: $searchString
+    )
+  }
+}
+
+#Preview("firstSeen", traits: .vaultModel) {
+  @Previewable @Environment(VaultModel.self) var model
+  @Previewable @State var searchString = ""
+
+  NavigationStack {
+    ArtistList(
+      artistDigests: model.vault.artistDigests, sectioner: model.vault.sectioner,
+      sort: .firstSeen, searchString: $searchString
+    )
+  }
 }
