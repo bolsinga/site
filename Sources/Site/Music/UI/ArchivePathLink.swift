bolsinga/site

commit 93ee346ba98828d6f28728c7191852c6196ed910

Link to ArchivePaths directly (#1254)
this reduces one level of abstraction, hopefully to slim down the amount of data necessary to show data.

Sources/Site/Music/UI/ArchivePathLink.swift
@@ -1,0 +1,63 @@
+//
+//  ArchivePathLink.swift
+//
+//
+//  Created by Greg Bolsinga on 8/22/24.
+//
+
+import SwiftUI
+
+struct ArchivePathLink<Label>: View where Label: View {
+  private let archivePath: ArchivePath
+  private let isPathNavigable: (ArchivePath) -> Bool
+  @ViewBuilder private let labelBuilder: (() -> Label)?
+  private let title: (any StringProtocol)?
+
+  private init(
+    archivePath: ArchivePath, isPathNavigable: @escaping (ArchivePath) -> Bool,
+    labelBuilder: (() -> Label)?, title: (any StringProtocol)?
+  ) {
+    self.archivePath = archivePath
+    self.isPathNavigable = isPathNavigable
+    self.labelBuilder = labelBuilder
+    self.title = title
+  }
+
+  init(
+    archivePath: ArchivePath, isPathNavigable: @escaping (ArchivePath) -> Bool,
+    labelBuilder: @escaping (() -> Label)
+  ) {
+    self.init(
+      archivePath: archivePath, isPathNavigable: isPathNavigable, labelBuilder: labelBuilder,
+      title: nil)
+  }
+
+  init(
+    archivePath: ArchivePath, isPathNavigable: @escaping (ArchivePath) -> Bool,
+    title: any StringProtocol
+  ) where Label == Never {
+    self.init(
+      archivePath: archivePath, isPathNavigable: isPathNavigable, labelBuilder: nil,
+      title: title)
+  }
+
+  @ViewBuilder private var labelView: some View {
+    if let labelBuilder {
+      labelBuilder()
+    } else if let title {
+      Text(title)
+    }
+  }
+
+  var body: some View {
+    if isPathNavigable(archivePath) {
+      if let title {
+        NavigationLink(title, archivePath: archivePath)
+      } else {
+        NavigationLink(archivePath: archivePath) { labelView }
+      }
+    } else {
+      labelView
+    }
+  }
+}

Sources/Site/Music/UI/NavigationLink+ArchivePath.swift
@@ -1,0 +1,18 @@
+//
+//  NavigationLink+ArchivePath.swift
+//  SiteApp
+//
+//  Created by Greg Bolsinga on 6/10/23.
+//
+
+import SwiftUI
+
+extension NavigationLink where Destination == Never {
+  init(archivePath: ArchivePath, @ViewBuilder label: () -> Label) {
+    self.init(value: archivePath, label: label)
+  }
+
+  init<S>(_ title: S, archivePath: ArchivePath) where Label == Text, S: StringProtocol {
+    self.init(title, value: archivePath)
+  }
+}

SiteApp.xcodeproj/project.pbxproj
@@ -99,8 +99,8 @@
 				"MusicData/ArchivePath+ArchiveCategory.swift",
 				"MusicData/ArchivePath+Codable.swift",
 				"MusicData/ArchivePath+FormatStyle.swift",
+				"MusicData/ArchivePath+Navigable.swift",
 				"MusicData/ArchivePath+ParseableFormatStyle.swift",
-				"MusicData/ArchivePath+PathRestorable.swift",
 				"MusicData/ArchivePath+URL.swift",
 				MusicData/Artist.swift,
 				"MusicData/Artist+Digest.swift",
@@ -185,8 +185,8 @@
 				"MusicData/ArchivePath+ArchiveCategory.swift",
 				"MusicData/ArchivePath+Codable.swift",
 				"MusicData/ArchivePath+FormatStyle.swift",
+				"MusicData/ArchivePath+Navigable.swift",
 				"MusicData/ArchivePath+ParseableFormatStyle.swift",
-				"MusicData/ArchivePath+PathRestorable.swift",
 				"MusicData/ArchivePath+URL.swift",
 				MusicData/Artist.swift,
 				"MusicData/Artist+Digest.swift",

Sources/MusicData/ArchivePath+Navigable.swift
@@ -1,5 +1,5 @@
 //
-//  ArchivePath+PathRestorable.swift
+//  ArchivePath+Navigable.swift
 //
 //
 //  Created by Greg Bolsinga on 9/6/24.
@@ -8,8 +8,7 @@
 import Foundation
 
 extension Array where Element == ArchivePath {
-  public func isPathNavigable(_ pathRestorable: PathRestorable) -> Bool {
-    let archivePath = pathRestorable.archivePath
+  public func isPathNavigable(_ archivePath: ArchivePath) -> Bool {
     // Drop the last path so that when going back the state is correct. Otherwise the '>' will flash on after animating in.
     return !self.dropLast().contains { $0 == archivePath }
   }

Sources/Site/Music/UI/ArchivePath+Destination.swift
@@ -11,7 +11,7 @@ import SwiftUI
 extension ArchivePath {
   @MainActor
   @ViewBuilder func destination(
-    vault: Vault, isPathNavigable: @escaping (PathRestorable) -> Bool,
+    vault: Vault, isPathNavigable: @escaping (ArchivePath) -> Bool,
     geocoder: @escaping @MainActor (VenueDigest) async throws -> MKMapItem?
   )
     -> some View

Sources/Site/Music/UI/ArtistDetail.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ArtistDetail: View {
   let digest: ArtistDigest
   let concertCompare: (Concert, Concert) -> Bool
-  let isPathNavigable: (PathRestorable) -> Bool
+  let isPathNavigable: (ArchivePath) -> Bool
 
   @ViewBuilder private var firstSetElement: some View {
     HStack {
@@ -35,7 +35,7 @@ struct ArtistDetail: View {
         header: Text("Shows")
       ) {
         ForEach(digest.concerts.sorted(by: concertCompare)) { concert in
-          PathRestorableLink(pathRestorable: concert, isPathNavigable: isPathNavigable) {
+          ArchivePathLink(archivePath: concert.archivePath, isPathNavigable: isPathNavigable) {
             ArtistBlurb(
               count: concert.show.artists.count, venue: concert.venue?.name, date: concert.show.date
             )
@@ -51,8 +51,8 @@ struct ArtistDetail: View {
         header: Text("Related Artists")
       ) {
         ForEach(digest.related) { relatedArtist in
-          PathRestorableLink(
-            pathRestorable: relatedArtist, isPathNavigable: isPathNavigable,
+          ArchivePathLink(
+            archivePath: relatedArtist.archivePath, isPathNavigable: isPathNavigable,
             title: relatedArtist.name)
         }
       }
@@ -107,7 +107,7 @@ struct ArtistDetail: View {
     ArtistDetail(
       digest: digest,
       concertCompare: model.vault.comparator.compare(lhs:rhs:),
-      isPathNavigable: { $0.archivePath != selectedConcert.archivePath }
+      isPathNavigable: { $0 != selectedConcert.archivePath }
     )
   }
 }

Sources/Site/Music/UI/NavigationLink+PathRestorable.swift
@@ -1,18 +1,0 @@
-//
-//  NavigationLink+PathRestorable.swift
-//
-//
-//  Created by Greg Bolsinga on 6/10/23.
-//
-
-import SwiftUI
-
-extension NavigationLink where Destination == Never {
-  init(value: PathRestorable, @ViewBuilder label: () -> Label) {
-    self.init(value: value.archivePath, label: label)
-  }
-
-  init<S>(_ title: S, value: PathRestorable) where Label == Text, S: StringProtocol {
-    self.init(title, value: value.archivePath)
-  }
-}

Sources/Site/Music/UI/PathRestorableLink.swift
@@ -1,63 +1,0 @@
-//
-//  PathRestorableLink.swift
-//
-//
-//  Created by Greg Bolsinga on 8/22/24.
-//
-
-import SwiftUI
-
-struct PathRestorableLink<Label>: View where Label: View {
-  private let pathRestorable: PathRestorable
-  private let isPathNavigable: (PathRestorable) -> Bool
-  @ViewBuilder private let labelBuilder: (() -> Label)?
-  private let title: (any StringProtocol)?
-
-  private init(
-    pathRestorable: any PathRestorable, isPathNavigable: @escaping (any PathRestorable) -> Bool,
-    labelBuilder: (() -> Label)?, title: (any StringProtocol)?
-  ) {
-    self.pathRestorable = pathRestorable
-    self.isPathNavigable = isPathNavigable
-    self.labelBuilder = labelBuilder
-    self.title = title
-  }
-
-  init(
-    pathRestorable: any PathRestorable, isPathNavigable: @escaping (any PathRestorable) -> Bool,
-    labelBuilder: @escaping (() -> Label)
-  ) {
-    self.init(
-      pathRestorable: pathRestorable, isPathNavigable: isPathNavigable, labelBuilder: labelBuilder,
-      title: nil)
-  }
-
-  init(
-    pathRestorable: any PathRestorable, isPathNavigable: @escaping (any PathRestorable) -> Bool,
-    title: any StringProtocol
-  ) where Label == Never {
-    self.init(
-      pathRestorable: pathRestorable, isPathNavigable: isPathNavigable, labelBuilder: nil,
-      title: title)
-  }
-
-  @ViewBuilder private var labelView: some View {
-    if let labelBuilder {
-      labelBuilder()
-    } else if let title {
-      Text(title)
-    }
-  }
-
-  var body: some View {
-    if isPathNavigable(pathRestorable) {
-      if let title {
-        NavigationLink(title, value: pathRestorable)
-      } else {
-        NavigationLink(value: pathRestorable) { labelView }
-      }
-    } else {
-      labelView
-    }
-  }
-}

commit 363a057fccceb905e7066eb387f2832941d46ede

Fix broken links!
broken by #1254. remove NavigationLink extensions, and pass ArchivePaths!

Sources/Site/Music/UI/ArchivePathLink.swift
@@ -52,9 +52,9 @@ struct ArchivePathLink<Label>: View where Label: View {
   var body: some View {
     if isPathNavigable(archivePath) {
       if let title {
-        NavigationLink(title, archivePath: archivePath)
+        NavigationLink(title, value: archivePath)
       } else {
-        NavigationLink(archivePath: archivePath) { labelView }
+        NavigationLink(value: archivePath) { labelView }
       }
     } else {
       labelView

Sources/Site/Music/UI/DayList.swift
@@ -28,7 +28,7 @@ struct DayList: View {
         )
       } else {
         List(concerts) { concert in
-          NavigationLink(value: concert) {
+          NavigationLink(value: concert.archivePath) {
             ConcertBlurb(
               venue: concert.venue?.name, date: concert.show.date, performers: concert.performers,
               dateFormat: .relative)

Sources/Site/Music/UI/NavigationLink+ArchivePath.swift
@@ -1,18 +1,0 @@
-//
-//  NavigationLink+ArchivePath.swift
-//  SiteApp
-//
-//  Created by Greg Bolsinga on 6/10/23.
-//
-
-import SwiftUI
-
-extension NavigationLink where Destination == Never {
-  init(archivePath: ArchivePath, @ViewBuilder label: () -> Label) {
-    self.init(value: archivePath, label: label)
-  }
-
-  init<S>(_ title: S, archivePath: ArchivePath) where Label == Text, S: StringProtocol {
-    self.init(title, value: archivePath)
-  }
-}

Sources/Site/Music/UI/RankingList.swift
@@ -23,7 +23,7 @@ where T: LibraryComparable, T: Hashable, T: PathRestorable, R: Comparable, R: Ha
         if let items = rankingMap[ranking] {
           Section {
             ForEach(items) { item in
-              NavigationLink(value: item) {
+              NavigationLink(value: item.archivePath) {
                 LabeledContent {
                   itemContentView(item)
                 } label: {

Sources/Site/Music/UI/ShowYearList.swift
@@ -33,7 +33,7 @@ struct ShowYearList: View {
             }, id: \.self
           ) { annum in
             let concerts = decadeMap[annum] ?? []
-            NavigationLink(value: annum) {
+            NavigationLink(value: annum.archivePath) {
               LabeledContent(
                 annum.formatted(),
                 value: String(localized: "\(concerts.count) Show(s)"))