bolsinga/site

commit 8d59731d2c358f5366185eb63457682cdb323480

add convenience methods for global search (#1278)
this uses the maps instead of the arrays.

Sources/Site/Music/UI/ArchiveCrossSearchView.swift
@@ -23,6 +23,16 @@ private struct SearchResultButton: View {
   }
 }
 
+extension Vault {
+  fileprivate func artistDigests(filteredBy searchString: String) -> [ArtistDigest] {
+    artistDigestMap.values.names(filteredBy: searchString, additive: true)
+  }
+
+  fileprivate func venueDigests(filteredBy searchString: String) -> [VenueDigest] {
+    venueDigestMap.values.names(filteredBy: searchString, additive: true)
+  }
+}
+
 struct ArchiveCrossSearchView: View {
   @Environment(VaultModel.self) var model
   @AppStorage("nearby.distance") private var nearbyDistance = defaultNearbyDistanceThreshold
@@ -36,14 +46,14 @@ struct ArchiveCrossSearchView: View {
     case .venue:
       []
     case .all, .artist:
-      model.vault.artistDigests.names(filteredBy: searchString, additive: true)
+      model.vault.artistDigests(filteredBy: searchString)
     }
   }
 
   private var venueDigests: [VenueDigest] {
     switch scope {
     case .all, .venue:
-      model.vault.venueDigests.names(filteredBy: searchString, additive: true)
+      model.vault.venueDigests(filteredBy: searchString)
     case .artist:
       []
     }

commit 2a855046b17a7fb417c0c3633c1366455968c823

Sort the cross search result output
broken by #1278

Sources/Site/Music/UI/ArchiveCrossSearchView.swift
@@ -25,11 +25,11 @@ private struct SearchResultButton: View {
 
 extension Vault {
   fileprivate func artistDigests(filteredBy searchString: String) -> [ArtistDigest] {
-    artistDigestMap.values.names(filteredBy: searchString, additive: true)
+    artistDigestMap.values.names(filteredBy: searchString, additive: true).sorted(by: comparator.libraryCompare(lhs:rhs:))
   }
 
   fileprivate func venueDigests(filteredBy searchString: String) -> [VenueDigest] {
-    venueDigestMap.values.names(filteredBy: searchString, additive: true)
+    venueDigestMap.values.names(filteredBy: searchString, additive: true).sorted(by: comparator.libraryCompare(lhs:rhs:))
   }
 }
 