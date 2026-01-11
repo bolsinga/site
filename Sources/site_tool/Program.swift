e08d9c11d0442bf36c75ef50630e1f91baf7dae4
Greg Bolsinga <bolsinga@mac.com>
2026-01-11 09:07:09 -0800

    Add DumpVault subcommand

--- Sources/site_tool/Program.swift
+++ Sources/site_tool/Program.swift
@@ -1,8 +1,8 @@
 //
-//  Program.swift
-//  site
+//  DumpVaultCommand.swift
+//  site_tool
 //
-//  Created by Greg Bolsinga on 12/6/20.
+//  Created by Greg Bolsinga on 1/11/26.
 //
 
 import ArgumentParser
@@ -10,47 +10,12 @@ import Foundation
 
 @main
 struct Program: AsyncParsableCommand {
-  @OptionGroup var rootURL: RootURLArguments
-
-  @MainActor
-  func run() async throws {
-    let vault = try await rootURL.vault()
-
-    let concerts = vault.concertMap.values.sorted(by: vault.comparator.compare(lhs:rhs:))
-    let artistDigests = vault.artistDigestMap.values.sorted(
-      by: vault.comparator.libraryCompare(lhs:rhs:))
-    let venueDigests = vault.venueDigestMap.values.sorted(
-      by: vault.comparator.libraryCompare(lhs:rhs:))
-
-    print("Artists: \(artistDigests.count)")
-    print("Shows: \(concerts.count)")
-    print("Venues: \(venueDigests.count)")
-
-    for concert in concerts.reversed() {
-      print(concert.formatted(.full))
-    }
-
-    for digest in venueDigests {
-      print(digest.venue.formatted(.oneLine))
-    }
-
-    for digest in artistDigests {
-      let concerts = digest.shows.compactMap {
-        switch $0.id {
-        case .show(let iD):
-          return vault.concertMap[iD]
-        default:
-          return nil
-        }
-      }
-
-      guard !concerts.isEmpty else { continue }
-
-      var concertParts: [String] = []
-      for concert in concerts {
-        concertParts.append(concert.formatted(.full))
-      }
-      print("\(digest.artist.name): (\(concertParts.joined(separator: "; "))")
-    }
-  }
+  public static let configuration = CommandConfiguration(
+    commandName: "site_tool",
+    abstract: "A tool for working with site data.",
+    subcommands: [
+      DumpVaultCommand.self
+    ],
+    defaultSubcommand: DumpVaultCommand.self
+  )
 }
