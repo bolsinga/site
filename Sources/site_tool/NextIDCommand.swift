83c79711d0fc60357f2d9769291c60252e0d90c4
Greg Bolsinga <bolsinga@mac.com>
2026-01-11 10:25:24 -0800

    NextID is now just a SubCommand instead of a full Target (#1309)
    much simpler!

---
+++ Sources/site_tool/NextIDCommand.swift
@@ -0,0 +1,42 @@
+//
+//  NextIDCommand.swift
+//
+//  Created by Greg Bolsinga on 8/3/23.
+//
+
+import ArgumentParser
+import Foundation
+
+extension Vault {
+  fileprivate var nextShowID: Show.ID {
+    let nextIndex = max(concertMap.count, 0)
+    return "\(ArchivePath.showPrefix)\(nextIndex)"
+  }
+
+  fileprivate var nextVenueID: Venue.ID {
+    let nextIndex = max(venueDigestMap.count, 0)
+    return "\(ArchivePath.venuePrefix)\(nextIndex)"
+  }
+
+  fileprivate var nextArtistID: Venue.ID {
+    let nextIndex = max(artistDigestMap.count, 0)
+    return "\(ArchivePath.artistPrefix)\(nextIndex)"
+  }
+}
+
+struct NextIDCommand: AsyncParsableCommand {
+  static let configuration = CommandConfiguration(
+    commandName: "nextID",
+    abstract: "Prints the next ID for all the music entires."
+  )
+
+  @OptionGroup var rootURL: RootURLArguments
+
+  func run() async throws {
+    let vault = try await rootURL.vault()
+
+    print("Next Show: \(vault.nextShowID)")
+    print("Next Venue: \(vault.nextVenueID)")
+    print("Next Artist: \(vault.nextArtistID)")
+  }
+}
--- Sources/site_tool/Program.swift
+++ Sources/site_tool/Program.swift
@@ -14,7 +14,7 @@ struct Program: AsyncParsableCommand {
     commandName: "site_tool",
     abstract: "A tool for working with site data.",
     subcommands: [
-      DumpVaultCommand.self, DumpDiaryCommand.self, DumpBracketCommand.self,
+      DumpVaultCommand.self, DumpDiaryCommand.self, DumpBracketCommand.self, NextIDCommand.self
     ],
     defaultSubcommand: DumpVaultCommand.self
   )
