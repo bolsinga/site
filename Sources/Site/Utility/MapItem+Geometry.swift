38c4e1c4b39c601bcf7b75d76f5f0145104ee4f4
Greg Bolsinga <bolsinga@mac.com>
2026-06-07 10:42:33 -0700

    improve map rect padding geometry
    - add some debug ui to the location map.
    - useful for upcoming map of all venues.

--- Sources/Site/Utility/MapItem+Geometry.swift
+++ Sources/Site/Utility/MapItem+Geometry.swift
@@ -7,26 +7,76 @@
 
 import MapKit
 
-extension MKMapItem {
-  var center: CLLocationCoordinate2D {
-    self.location.coordinate
+extension MKMapRect {
+  fileprivate var centerPoint: MKMapPoint {
+    MKMapPoint(x: midX, y: midY)
   }
-  var radius: CLLocationDistance {
-    // FB19027378 No region for a MKMapItem
-    100
+
+  fileprivate var centerCoordinate: CLLocationCoordinate2D {
+    centerPoint.coordinate
+  }
+
+  fileprivate func padded(_ distance: CLLocationDistance = 100.0) -> MKMapRect {
+    let mapPointOffset = MKMapPointsPerMeterAtLatitude(centerCoordinate.latitude) * distance / 2.0
+
+    return insetBy(dx: -mapPointOffset, dy: -mapPointOffset)
+  }
+
+  var proportionallyPadded: MKMapRect {
+    let midY = (maxY + minY) / 2.0
+
+    let midLeft = MKMapPoint(x: minX, y: midY)
+    let midRight = MKMapPoint(x: maxX, y: midY)
+
+    let widthInMeters = midLeft.distance(to: midRight)
+
+    if widthInMeters < 100.0 {
+      return padded()
+    }
+
+    return padded(widthInMeters * 0.1)
+  }
+
+  fileprivate var topLeft: MKMapPoint {
+    MKMapPoint(x: minX, y: maxY)
+  }
+
+  fileprivate var topRight: MKMapPoint {
+    MKMapPoint(x: maxX, y: maxY)
+  }
+
+  fileprivate var bottomRight: MKMapPoint {
+    MKMapPoint(x: maxX, y: minY)
+  }
+
+  fileprivate var bottomLeft: MKMapPoint {
+    MKMapPoint(x: minX, y: minY)
+  }
+
+  var corners: [MKMapPoint] {
+    [topLeft, topRight, bottomRight, bottomLeft]
+  }
+}
+
+extension MKMapItem {
+  fileprivate var center: CLLocationCoordinate2D {
+    self.location.coordinate
   }
 
   var rect: MKMapRect {
-    let center = center
+    let centerCoordinate = center
+    guard CLLocationCoordinate2DIsValid(centerCoordinate) else { return .null }
 
-    let mapPointOffset = MKMapPointsPerMeterAtLatitude(center.latitude) * radius / 2.0
-
-    return MKMapRect(origin: MKMapPoint(center), size: MKMapSize(width: 1, height: 1)).insetBy(
-      dx: -mapPointOffset, dy: -mapPointOffset)
+    let unitSize = MKMapSize(width: 1.0, height: 1.0)
+    let centerPoint = MKMapPoint(centerCoordinate)
+    let originPoint = MKMapPoint(
+      x: centerPoint.x - unitSize.width / 2.0, y: centerPoint.y - unitSize.height / 2.0)
+    return MKMapRect(origin: originPoint, size: unitSize)
   }
+}
 
-  var paddedRect: MKMapRect {
-    let locatableRect = self.rect
-    return locatableRect.insetBy(dx: -locatableRect.width / 10.0, dy: -locatableRect.height / 10.0)
+extension Collection where Element == MKMapItem {
+  var rect: MKMapRect {
+    reduce(into: MKMapRect.null) { $0 = $0.union($1.rect) }
   }
 }
